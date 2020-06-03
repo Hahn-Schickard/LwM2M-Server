7f4a0f8937e23c4df95196a5c272b5b5ae043015
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2020-06-03 08:14:03 +0200

    add print functions fro Token, Options and Payload

--- sources/Runner/main.cpp
+++ sources/Runner/main.cpp
@@ -18,14 +18,49 @@ void printCoAPHeader(CoAP_Header header) {
   cout << "Message id is: " << header.getMessageID() << endl;
 }
 
+void printCoAPToken(vector<uint8_t> token) {
+  cout << "With Token:" << endl;
+  for (uint8_t byte : token) {
+    cout << to_string(byte) << " ";
+  }
+  cout << endl;
+}
+
+void printCoAPOptions(vector<shared_ptr<CoAP_Option>> options) {
+  cout << "With " << options.size() << " Options" << endl;
+  for (auto option : options) {
+    cout << (option->isCritical() ? "Critical" : "Non-critical");
+    cout << (option->isRepeatable() ? "Repeatable" : "Non-repeatabe");
+    cout << (option->isUnsafe() ? "Unsafe" : "Safe");
+    cout << option->size() << "Byte long option: ";
+    cout << toString(option->getOptionNumber()) << endl;
+  }
+}
+
+void printCoAPPayload(vector<uint8_t> payload) {
+  cout << "With Data:" << endl;
+  for (uint8_t byte : payload) {
+    cout << to_string(byte) << " ";
+  }
+  cout << endl;
+}
+
 void printCoAPMessage(shared_ptr<CoAP_Message> message) {
   cout << "Received a message from: " << message->getReceiverIP() << ":"
        << message->getReceiverPort() << endl;
   printCoAPHeader(message->getHeader());
-  for (const auto &byte : message->getBody()) {
-    cout << byte << " ";
+
+  if (!message->getToken().empty()) {
+    printCoAPToken(message->getToken());
   }
-  cout << endl;
+
+  if (!message->getOptions().empty()) {
+    printCoAPOptions(message->getOptions());
+  }
+
+  if (!message->getBody().empty()) {
+    printCoAPPayload(message->getBody());
+  }
 }
 
 CoAP_Message makeDummyMessage(const string &receiver_address, unsigned int port,
@@ -44,17 +79,17 @@ int main() {
   thread server_thread;
   logger->log(SeverityLevel::INFO, "Started LwM2M Server!");
   LwM2M_Server::CoAP_Server server(false, 16833, 10);
-  thread client_thread;
-  LwM2M_Client::DummyClient client(false, 16835);
+  // thread client_thread;
+  // LwM2M_Client::DummyClient client(false, 16835);
   try {
     server_thread = thread([&]() { server.run(); });
-    client_thread = thread([&]() {
-      int message_id = 0;
-      do {
-        client.sendMessage(makeDummyMessage("0.0.0.0", 16833, message_id));
-        message_id++;
-      } while (!server.stopRequested());
-    });
+    // client_thread = thread([&]() {
+    //   int message_id = 0;
+    //   do {
+    //     client.sendMessage(makeDummyMessage("0.0.0.0", 16833, message_id));
+    //     message_id++;
+    //   } while (!server.stopRequested());
+    //});
 
     do {
       printCoAPMessage(server.pullRequest());
@@ -67,6 +102,6 @@ int main() {
   }
 
   server_thread.join();
-  client_thread.join();
+  // client_thread.join();
   exit(EXIT_SUCCESS);
 }
