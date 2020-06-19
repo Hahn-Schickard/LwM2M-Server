9a26b6855f9172d280cd3f601e7c28080be6bbd4
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2020-06-19 17:33:07 +0200

    implement first draft of LwM2M Device construction

--- sources/LwM2M/Interfaces/RegIstrationInterface.cpp
+++ sources/LwM2M/Interfaces/RegIstrationInterface.cpp
@@ -18,11 +18,34 @@ RegistrationInterface::RegistrationInterface(const string &configuration_path)
   }
 }
 
+unordered_map<uint32_t, LwM2M_Object>
+RegistrationInterface::assignObjectInstances(
+    unordered_map<unsigned int, unsigned int> objects) {
+  unordered_map<uint32_t, LwM2M_Object> result;
+  for (auto &object : objects) {
+    uint32_t object_id = object.first;
+    uint32_t instance_id = object.second;
+    auto it = supported_descriptors_.find(object_id);
+    if (it != supported_descriptors_.end()) {
+      result.emplace(instance_id, it->second);
+    }
+  }
+  return result;
+}
+
 shared_ptr<LwM2M_Message> RegistrationInterface::handleRegisterRequest(
     shared_ptr<Register_Request> request) {
   shared_ptr<LwM2M_Message> result;
-  if (!request) {
-    LwM2M_Device new_device();
+  if (request) {
+    unordered_map<uint32_t, LwM2M_Object> object_instances =
+        assignObjectInstances(request->object_instances_map_);
+    LwM2M_Device new_device(request->endpoint_name_, request->endpoint_address_,
+                            request->endpoint_port_, request->life_time_,
+                            request->version_, request->binding_,
+                            request->queue_mode_, request->sms_number_,
+                            object_instances);
+    device_registery_->emplace(new_device.getDeviceId(), new_device);
+    result = make_shared<LwM2M_Message>();
   } else {
     result = make_shared<LwM2M_Message>();
   }
