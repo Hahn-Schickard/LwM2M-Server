ddc9581220222f073a3f64f8a897d1f9289f555f
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2020-06-19 10:53:00 +0200

    add documentation to XmlParser

--- includes/LwM2M/XML_Parser/XmlParser.hpp
+++ includes/LwM2M/XML_Parser/XmlParser.hpp
@@ -7,6 +7,14 @@
 
 namespace LwM2M_Model {
 std::string deserializeFile(const std::string &filepath);
+
+/**
+ * @brief Deserializes LwM2M XML Descriptors into the Object Model
+ *
+ * @param filepath - XML file containing a list of LwM2M Object descriptor
+ * locations
+ * @return std::vector<LwM2M_Model::LwM2M_Object>
+ */
 std::vector<LwM2M_Model::LwM2M_Object>
 deserializeModel(const std::string &filepath);
 } // namespace LwM2M_Model
