aab395a80ad53f4515525cbfb8dd09515bd9267f
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2020-06-19 14:14:33 +0200

    add first draft for LwM2M device

---
+++ includes/LwM2M/LwM2M_Model/LwM2M_Device.hpp
@@ -0,0 +1,33 @@
+#ifndef __LWM2M_DEVICE_HPP
+#define __LWM2M_DEVICE_HPP
+
+#include "LwM2M_Object.hpp"
+#include "RegistrationInterfaceMessages.hpp"
+
+#include <string>
+#include <unordered_map>
+
+namespace LwM2M_Model {
+class LwM2M_Device {
+  std::string device_id_;
+  std::string name_;
+  std::string endpoint_address_;
+  unsigned int endpoint_port_;
+  size_t life_time_;
+  LwM2M_Version version_;
+  BindingType binding_;
+  bool queue_mode_;
+  std::unordered_map<uint32_t, LwM2M_Object> object_instances_;
+
+public:
+  LwM2M_Device(std::string name, std::string endpoint_address,
+               unsigned int endpoint_port, size_t life_time,
+               LwM2M_Version version, BindingType binding, bool queue_mode,
+               std::optional<std::string> sms_number,
+               std::unordered_map<uint32_t, LwM2M_Object> object_instances_map);
+
+  std::string getDeviceId();
+};
+}; // namespace LwM2M_Model
+
+#endif //__LWM2M_DEVICE_HPP
