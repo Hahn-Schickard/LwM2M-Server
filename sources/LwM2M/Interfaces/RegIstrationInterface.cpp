d83e1fd9b061728852f16975d9a41436bcfdbcc7
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2020-06-19 17:06:58 +0200

    moved device id generation to LwM2M Device ctor

--- sources/LwM2M/Interfaces/RegIstrationInterface.cpp
+++ sources/LwM2M/Interfaces/RegIstrationInterface.cpp
@@ -7,13 +7,6 @@ using namespace HaSLL;
 
 namespace LwM2M_Model {
 
-string generateDeviceID(string name, string endpoint_address,
-                        unsigned int endpoint_port) {
-  size_t result =
-      hash<string>{}(name) + hash<string>{}(endpoint_address) + endpoint_port;
-  return to_string(result);
-}
-
 RegistrationInterface::RegistrationInterface(const string &configuration_path)
     : logger_(LoggerRepository::getInstance().registerTypedLoger(this)) {
   try {
@@ -28,7 +21,11 @@ RegistrationInterface::RegistrationInterface(const string &configuration_path)
 shared_ptr<LwM2M_Message> RegistrationInterface::handleRegisterRequest(
     shared_ptr<Register_Request> request) {
   shared_ptr<LwM2M_Message> result;
-  result = make_shared<LwM2M_Message>();
+  if (!request) {
+    LwM2M_Device new_device();
+  } else {
+    result = make_shared<LwM2M_Message>();
+  }
   return result;
 }
 
@@ -49,7 +46,7 @@ shared_ptr<LwM2M_Message> RegistrationInterface::handleDeregisterRequest(
 shared_ptr<LwM2M_Message> RegistrationInterface::handleRequest(
     shared_ptr<Regirstration_Interface_Message> message) {
   shared_ptr<LwM2M_Message> result;
-  switch (message->getMessageType()) {
+  switch (message->message_type_) {
   case MessageType::REGISTER: {
     result =
         handleRegisterRequest(static_pointer_cast<Register_Request>(message));
@@ -66,7 +63,7 @@ shared_ptr<LwM2M_Message> RegistrationInterface::handleRequest(
   }
   default: {
     logger_->log(SeverityLevel::ERROR, "Could not process {} message type",
-                 message->getMessageType());
+                 message->message_type_);
     result = make_shared<LwM2M_Message>();
     break;
   }
