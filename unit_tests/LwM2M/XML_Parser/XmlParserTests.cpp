505605de52e63a172c4087a2c9b5c95820b45091
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2020-06-19 11:36:21 +0200

    add more XmlParser tests

--- unit_tests/LwM2M/XML_Parser/XmlParserTests.cpp
+++ unit_tests/LwM2M/XML_Parser/XmlParserTests.cpp
@@ -7,8 +7,33 @@ using namespace std;
 using namespace LwM2M_Model;
 
 TEST(XmlParserTests, canDeserializeModel) {
-  ASSERT_NO_THROW({
-    vector<LwM2M_Object> lwm2m_model =
-        deserializeModel("unit_tests/LwM2M/XML_Parser/model/ipsoConfig.xml");
-  });
+  EXPECT_NO_THROW(deserializeModel("model/passingModel1.xml"));
+}
+
+TEST(XmlParserTests, thorwsExceptionOnNonExistantConfigFile) {
+  EXPECT_THROW(deserializeModel("model/doesNotExist.xml"), runtime_error);
+}
+
+TEST(XmlParserTests, thorwsExceptionOnNonExistantDescriptor) {
+  EXPECT_THROW(deserializeModel("model/faillingModel1.xml"), runtime_error);
+}
+
+TEST(XmlParserTests, canDeserializeDeviceObject) {
+  vector<LwM2M_Model::LwM2M_Object> model;
+  EXPECT_NO_THROW({ model = deserializeModel("model/passingModel2.xml"); });
+
+  EXPECT_EQ(model.size(), 1);
+
+  LwM2M_Object device_object = model.front();
+  EXPECT_EQ(device_object.id_, 3);
+  EXPECT_EQ(device_object.name_, "Device");
+  EXPECT_EQ(device_object.urn_, "urn:oma:lwm2m:oma:3");
+  EXPECT_EQ(device_object.multiple_instances_, false);
+  EXPECT_EQ(device_object.mandatory_, true);
+  EXPECT_EQ(device_object.resources_.size(), 23);
+
+  for (uint8_t expected_id = 0; expected_id < 23; expected_id++) {
+    auto resource = device_object.getResource(expected_id);
+    EXPECT_EQ(resource->id_, expected_id);
+  }
 }
