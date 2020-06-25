890abc6ea0f2729a1eb651151d9568b7886eb46e
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2020-06-25 15:23:55 +0200

    resolves #4

---
+++ includes/LwM2M/Message_Converter/MessageProcessorManager.hpp
@@ -0,0 +1,70 @@
+#include "Converter.hpp"
+#include "Stoppable.hpp"
+#include "Threadsafe_Queue.hpp"
+
+#include "CoAP_Message.hpp"
+#include "LoggerRepository.hpp"
+#include "LwM2M_Message.hpp"
+
+#include <memory>
+#include <thread>
+
+namespace LwM2M_Model {
+template <typename T> class MessageProcessorRunner : public Stoppable {
+  std::unique_ptr<Converter<T>> converter_;
+  std::shared_ptr<ThreadsafeQueue<T>> message_buffer_;
+  std::shared_ptr<HaSLL::Logger> logger_;
+
+public:
+  MessageProcessorRunner() {}
+
+  MessageProcessorRunner(std::unique_ptr<Converter<T>> converter,
+                         std::shared_ptr<ThreadsafeQueue<T>> message_buffer,
+                         std::string logger_name)
+      : Stoppable(), converter_(move(converter)),
+        message_buffer_(message_buffer),
+        logger_(
+            HaSLL::LoggerRepository::getInstance().registerLoger(logger_name)) {
+    run();
+  }
+
+  void run() override {
+    while (!stopRequested()) {
+      try {
+        converter_->convert(message_buffer_->try_pop());
+      } catch (std::exception &ex) {
+        logger_->log(HaSLL::SeverityLevel::ERROR, "Caught an exception: {}",
+                     ex.what());
+      }
+    }
+  }
+};
+
+template <typename T> struct MessageProcessor {
+  std::thread thread_id_;
+  MessageProcessorRunner<T> processor_;
+
+  MessageProcessor() {}
+  MessageProcessor(std::unique_ptr<Converter<T>> converter,
+                   std::shared_ptr<ThreadsafeQueue<T>> message_buffer,
+                   std::string logger_name)
+      : processor_(move(converter), message_buffer, logger_name) {
+    thread_id_ = std::thread([&]() { processor_.run(); });
+  }
+};
+
+class MessageProcessorManager {
+  std::shared_ptr<ThreadsafeQueue<LwM2M_Message>> outgoing_lwm2m_message_queue_;
+  MessageProcessor<CoAP::CoAP_Message> incoming_message_processing_thread_;
+  MessageProcessor<LwM2M_Message> outgoing_message_processing_thread_;
+
+public:
+  MessageProcessorManager(std::shared_ptr<ThreadsafeQueue<CoAP::CoAP_Message>>
+                              incoming_message_queue,
+                          std::shared_ptr<ThreadsafeQueue<LwM2M_Message>>
+                              outgoing_lwm2m_message_queue,
+                          std::shared_ptr<ThreadsafeQueue<CoAP::CoAP_Message>>
+                              outgoing_message_queue);
+  ~MessageProcessorManager();
+};
+} // namespace LwM2M_Model
--- sources/LwM2M/Message_Converter/MessageProcessorManager.cpp
+++ sources/LwM2M/Message_Converter/MessageProcessorManager.cpp
@@ -1,6 +1,6 @@
-#include "MessageProcessingThreadPool.hpp"
 #include "CoAP_To_LwM2M.hpp"
 #include "LwM2M_To_CoAP.hpp"
+#include "MessageProcessorManager.hpp"
 
 #include <cctype>
 #include <optional>
@@ -11,7 +11,7 @@ using namespace std;
 using namespace CoAP;
 namespace LwM2M_Model {
 
-MessageProcessingThreadPool::MessageProcessingThreadPool(
+MessageProcessorManager::MessageProcessorManager(
     shared_ptr<ThreadsafeQueue<CoAP_Message>> incoming_message_queue,
     shared_ptr<ThreadsafeQueue<LwM2M_Message>> outgoing_lwm2m_message_queue,
     shared_ptr<ThreadsafeQueue<CoAP_Message>> outgoing_message_queue)
@@ -23,7 +23,7 @@ MessageProcessingThreadPool::MessageProcessingThreadPool(
           make_unique<LwM2M_To_CoAP>(outgoing_message_queue),
           outgoing_lwm2m_message_queue, "OutgoingMessageProcessor") {}
 
-MessageProcessingThreadPool::~MessageProcessingThreadPool() {
+MessageProcessorManager::~MessageProcessorManager() {
   incoming_message_processing_thread_.processor_.stop();
   outgoing_message_processing_thread_.processor_.stop();
   // possible race condition at this point?
