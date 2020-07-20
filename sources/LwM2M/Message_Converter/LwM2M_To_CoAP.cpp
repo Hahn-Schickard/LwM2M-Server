38463c688392205e5e7758fd524d4ca6fab897d9
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2020-07-20 13:15:28 +0200

    simplify LwM2M 2 CoAP converter

--- sources/LwM2M/Message_Converter/LwM2M_To_CoAP.cpp
+++ sources/LwM2M/Message_Converter/LwM2M_To_CoAP.cpp
@@ -1,115 +1,41 @@
 #include "LwM2M_To_CoAP.hpp"
+#include "ContentFormat.hpp"
+#include "PlainText.hpp"
+#include "RegistrationInterfaceMessages.hpp"
+#include "UniquePtrCast.hpp"
 
 using namespace std;
 using namespace CoAP;
 namespace LwM2M {
 
-unique_ptr<CoAP::Message> makeFromRegisterMessage(shared_ptr<Message> input) {
-  return unique_ptr<CoAP::Message>();
+unique_ptr<CoAP::Message> createResponse(unique_ptr<Response> input) {
+  uint16_t message_id;
+  auto payload = make_shared<CoAP::PlainText>(input->payload_);
+  vector<std::shared_ptr<CoAP::Option>> options{
+      make_shared<CoAP::ContentFormat>(CoAP::ContentFormatType::PLAIN_TEXT)};
+  return make_unique<CoAP::Message>(
+      input->endpoint_address_, input->endpoint_port_,
+      CoAP::Header(CoAP::MessageType::ACKNOWLEDGMENT, payload->size(),
+                   static_cast<CoAP::CodeType>(input->response_code_),
+                   input->message_id_),
+      input->token_, options, move(payload));
 }
 
-unique_ptr<CoAP::Message> makeFromDeRegisterMessage(shared_ptr<Message> input) {
+unique_ptr<CoAP::Message> createRequest(unique_ptr<Message> input) {
+  return unique_ptr<CoAP::Message>();
 }
-unique_ptr<CoAP::Message> makeFromUpdateMessage(shared_ptr<Message> input) {}
-unique_ptr<CoAP::Message> makeFromReadMessage(shared_ptr<Message> input) {}
-unique_ptr<CoAP::Message> makeFromWriteMessage(shared_ptr<Message> input) {}
-unique_ptr<CoAP::Message> makeFromExecuteMessage(shared_ptr<Message> input) {}
-unique_ptr<CoAP::Message> makeFromCreateMessage(shared_ptr<Message> input) {}
-unique_ptr<CoAP::Message> makeFromDeleteMessage(shared_ptr<Message> input) {}
-unique_ptr<CoAP::Message>
-makeFromWriteAttributesMessage(shared_ptr<Message> input) {}
-unique_ptr<CoAP::Message> makeFromDiscoverMessage(shared_ptr<Message> input) {}
-unique_ptr<CoAP::Message>
-makeFromReadCompositeMessage(shared_ptr<Message> input) {}
-unique_ptr<CoAP::Message>
-makeFromWriteCompositeMessage(shared_ptr<Message> input) {}
-unique_ptr<CoAP::Message> makeFromObserveMessage(shared_ptr<Message> input) {}
-unique_ptr<CoAP::Message>
-makeFromObserveCompositeMessage(shared_ptr<Message> input) {}
-unique_ptr<CoAP::Message>
-makeFromCancelObersvationMessage(shared_ptr<Message> input) {}
-unique_ptr<CoAP::Message>
-makeFromCancelObersvationCompositeMessage(shared_ptr<Message> input) {}
 
 LwM2M_To_CoAP::LwM2M_To_CoAP(
     shared_ptr<ThreadsafeQueue<CoAP::Message>> output_queue)
     : output_queue_(output_queue) {}
 
-void LwM2M_To_CoAP::convert(shared_ptr<Message> message) {
+void LwM2M_To_CoAP::convert(unique_ptr<Message> message) {
   if (message) {
-    switch (message->message_type_) {
-    case MessageType::REGISTER: {
-      output_queue_->push(makeFromRegisterMessage(message));
-      break;
-    }
-    case MessageType::DEREGISTER: {
-      output_queue_->push(makeFromDeRegisterMessage(message));
-      break;
-    }
-    case MessageType::UPDATE: {
-      output_queue_->push(makeFromUpdateMessage(message));
-      break;
-    }
-    case MessageType::READ: {
-      output_queue_->push(makeFromReadMessage(message));
-      break;
-    }
-    case MessageType::WRITE: {
-      output_queue_->push(makeFromWriteMessage(message));
-      break;
-    }
-    case MessageType::EXECUTE: {
-      output_queue_->push(makeFromExecuteMessage(message));
-      break;
-    }
-    case MessageType::CREATE: {
-      output_queue_->push(makeFromCreateMessage(message));
-      break;
-    }
-    case MessageType::DELETE: {
-      output_queue_->push(makeFromDeleteMessage(message));
-      break;
-    }
-    case MessageType::WRITE_ATTRIBUTES: {
-      output_queue_->push(makeFromWriteAttributesMessage(message));
-      break;
-    }
-    case MessageType::DISCOVER: {
-      output_queue_->push(makeFromDiscoverMessage(message));
-      break;
-    }
-    case MessageType::READ_COMPOSITE: {
-      output_queue_->push(makeFromReadCompositeMessage(message));
-      break;
-    }
-    case MessageType::WRITE_COMPOSITE: {
-      output_queue_->push(makeFromWriteCompositeMessage(message));
-      break;
-    }
-    case MessageType::OBSERVE: {
-      output_queue_->push(makeFromObserveMessage(message));
-      break;
-    }
-    case MessageType::OBSERVE_COMPOSITE: {
-      output_queue_->push(makeFromObserveCompositeMessage(message));
-      break;
-    }
-    case MessageType::CANCEL_OBSERVATION: {
-      output_queue_->push(makeFromCancelObersvationMessage(message));
-      break;
-    }
-    case MessageType::CANCEL_OBSERVATION_COMPOSITE: {
-      output_queue_->push(makeFromCancelObersvationCompositeMessage(message));
-      break;
-    }
-    case MessageType::NOTIFY: {
-      // server does not issue notifications
-    }
-    case MessageType::SEND: {
-      // server does not issue sends
-    }
-    default: { break; }
-    }
+    if (message->response_)
+      output_queue_->push(createResponse(
+          utility::static_pointer_cast<Response>(move(message))));
+  } else {
+    output_queue_->push(createRequest(move(message)));
   }
 }
 
