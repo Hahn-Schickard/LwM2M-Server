71b97e02eea716bdd3f27d4e975d8c901eeb4cfc
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2020-08-11 13:49:13 +0200

    fix formatting and add documentation

--- includes/Threadsafe_Queue/Threadsafe_Queue.hpp
+++ includes/Threadsafe_Queue/Threadsafe_Queue.hpp
@@ -15,24 +15,24 @@
  */
 template <typename T> class ThreadsafeQueue {
 private:
-  struct node {
+  struct Node {
     std::unique_ptr<T> data;
-    std::unique_ptr<node> next;
+    std::unique_ptr<Node> next;
   };
 
   std::mutex head_mutex;
-  std::unique_ptr<node> head;
+  std::unique_ptr<Node> head;
   std::mutex tail_mutex;
-  node *tail;
+  Node *tail;
   std::condition_variable data_cond;
 
-  node *get_tail() {
+  Node *get_tail() {
     std::lock_guard<std::mutex> tail_lock(tail_mutex);
     return tail;
   }
 
-  std::unique_ptr<node> pop_head() {
-    std::unique_ptr<node> old_head = std::move(head);
+  std::unique_ptr<Node> pop_head() {
+    std::unique_ptr<Node> old_head = std::move(head);
     head = std::move(old_head->next);
     return old_head;
   }
@@ -43,47 +43,60 @@ private:
     return std::move(head_lock);
   }
 
-  std::unique_ptr<node> wait_pop_head() {
+  std::unique_ptr<Node> wait_pop_head() {
     std::unique_lock<std::mutex> head_lock(wait_for_data());
     return pop_head();
   }
 
-  std::unique_ptr<node> wait_pop_head(T &value) {
+  std::unique_ptr<Node> wait_pop_head(T &value) {
     std::unique_lock<std::mutex> head_lock(wait_for_data());
     value = std::move(*head->data);
     return pop_head();
   }
 
-  std::unique_ptr<node> try_pop_head() {
+  std::unique_ptr<Node> try_pop_head() {
     std::lock_guard<std::mutex> head_lock(head_mutex);
     if (head.get() == get_tail()) {
-      return std::unique_ptr<node>();
+      return std::unique_ptr<Node>();
     }
     return pop_head();
   }
 
-  std::unique_ptr<node> try_pop_head(T &value) {
+  std::unique_ptr<Node> try_pop_head(T &value) {
     std::lock_guard<std::mutex> head_lock(head_mutex);
     if (head.get() == get_tail()) {
-      return std::unique_ptr<node>();
+      return std::unique_ptr<Node>();
     }
     value = std::move(*head->data);
     return pop_head();
   }
 
 public:
-  ThreadsafeQueue() : head(new node), tail(head.get()) {}
+  ThreadsafeQueue() : head(new Node), tail(head.get()) {}
 
   ThreadsafeQueue(const ThreadsafeQueue &other) = delete;
   ThreadsafeQueue &operator=(const ThreadsafeQueue &other) = delete;
 
+  /**
+   * @brief Checks if the queue has some data to return
+   *
+   * @return std::unique_ptr<T>
+   */
   std::unique_ptr<T> try_pop() {
-    std::unique_ptr<node> old_head = try_pop_head();
+    std::unique_ptr<Node> old_head = try_pop_head();
     return old_head ? move(old_head->data) : std::unique_ptr<T>();
   }
 
+  /**
+   * @brief Checks if the queue has data to return, if so, it will populate a
+   * given buffer, otherwise it will not modify it
+   *
+   * @param value - buffer that will be populated
+   * @return true - populated buffer with data
+   * @return false - does not change the buffer
+   */
   bool try_pop(T &value) {
-    std::unique_ptr<node> const old_head = try_pop_head(value);
+    std::unique_ptr<Node> const old_head = try_pop_head(value);
     return old_head ? true : false;
   }
 
@@ -92,24 +105,45 @@ public:
     return (head.get() == get_tail());
   }
 
+  /**
+   * @brief Blocks the caller until the queue has some data to return
+   *
+   * @param value
+   */
   std::unique_ptr<T> wait_and_pop() {
-    std::unique_ptr<node> const old_head = wait_pop_head();
+    std::unique_ptr<Node> const old_head = wait_pop_head();
     return move(old_head->data);
   }
 
+  /**
+   * @brief Blocks the caller until the queue has some data to populate a given
+   * buffer
+   *
+   * @param value - buffer that will be populated
+   */
   void wait_and_pop(T &value) {
-    std::unique_ptr<node> const old_head = wait_pop_head(value);
+    std::unique_ptr<Node> const old_head = wait_pop_head(value);
   }
 
+  /**
+   * @brief Creates a std::unique_ptr from a given value and savs it in a queue
+   *
+   * @param new_value
+   */
   void push(T new_value) { push(std::make_unique<T>(std::move(new_value))); }
 
+  /**
+   * @brief Saves a given std::unique_ptr in a queue
+   *
+   * @param new_value
+   */
   void push(std::unique_ptr<T> new_value) {
     if (new_value) {
-      std::unique_ptr<node> p(new node);
+      std::unique_ptr<Node> p(new Node);
       {
         std::lock_guard<std::mutex> tail_lock(tail_mutex);
         tail->data = move(new_value);
-        node *const new_tail = p.get();
+        Node *const new_tail = p.get();
         tail->next = std::move(p);
         tail = new_tail;
       }
