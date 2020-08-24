2719260a2071e392bde2c2e4a1f66f818be3168a
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2020-08-24 13:55:04 +0200

    move variant visitor to its own header

--- includes/LwM2M/LwM2M_Model/LwM2M_Object.hpp
+++ includes/LwM2M/LwM2M_Model/LwM2M_Object.hpp
@@ -16,19 +16,6 @@ using ResourceVariant = std::variant<
     std::shared_ptr<Resource<uint64_t>>, std::shared_ptr<Resource<ObjectLink>>,
     std::shared_ptr<Resource<Opaque>>>;
 
-namespace detail {
-
-template <class... Ts> struct overloaded : Ts... { using Ts::operator()...; };
-
-template <class... Ts> overloaded(Ts...)->overloaded<Ts...>;
-
-} // namespace detail
-template <typename Variant, typename... Matchers>
-auto match(Variant &&variant, Matchers &&... matchers) {
-  return std::visit(detail::overloaded{std::forward<Matchers>(matchers)...},
-                    std::forward<Variant>(variant));
-}
-
 using ObjectDescriptorPtr = std::shared_ptr<ObjectDescriptor>;
 
 class Object {
