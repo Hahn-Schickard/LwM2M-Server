129f541b7d276145e37eb899ea35071477e3435f
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2020-08-24 18:42:27 +0200

    implement reponse decoding

--- sources/LwM2M/Message_Decoder/CoAP_Decoder.cpp
+++ sources/LwM2M/Message_Decoder/CoAP_Decoder.cpp
@@ -216,20 +216,96 @@ makeCancelObersvationCompositeMessage(const CoAP::Message *input) {}
 unique_ptr<Message> makeNotifyMessage(const CoAP::Message *input) {}
 unique_ptr<Message> makeSendMessage(const CoAP::Message *input) {}
 
+ResponseCode convert(CoAP::CodeType code_type) {
+  switch (code_type) {
+  case CoAP::CodeType::OK: {
+    return ResponseCode::OK;
+  }
+  case CoAP::CodeType::CREATED: {
+    return ResponseCode::CREATED;
+  }
+  case CoAP::CodeType::DELETED: {
+    return ResponseCode::DELETED;
+  }
+  case CoAP::CodeType::CHANGED: {
+    return ResponseCode::CHANGED;
+  }
+  case CoAP::CodeType::CONTENT: {
+    return ResponseCode::CONTENT;
+  }
+  case CoAP::CodeType::CONTINUE: {
+    return ResponseCode::CONTINUE;
+  }
+  case CoAP::CodeType::BAD_REQUEST: {
+    return ResponseCode::BAD_REQUEST;
+  }
+  case CoAP::CodeType::UNAUTHORIZED: {
+    return ResponseCode::UNAUTHORIZED;
+  }
+  case CoAP::CodeType::FORBIDDEN: {
+    return ResponseCode::FORBIDDEN;
+  }
+  case CoAP::CodeType::NOT_FOUND: {
+    return ResponseCode::NOT_FOUND;
+  }
+  case CoAP::CodeType::METHOD_NOT_ALLOWED: {
+    return ResponseCode::METHOD_NOT_ALLOWED;
+  }
+  case CoAP::CodeType::NOT_ACCEPTABLE: {
+    return ResponseCode::NOT_ACCEPTABLE;
+  }
+  case CoAP::CodeType::REQUEST_ENTITY_INCOMPLETE: {
+    return ResponseCode::REQUEST_ENTITY_TOO_LARGE;
+  }
+  case CoAP::CodeType::PRECOGNITION_FAILED: {
+    return ResponseCode::PRECOGNITION_FAILED;
+  }
+  case CoAP::CodeType::REQUEST_ENTITY_TOO_LARGE: {
+    return ResponseCode::REQUEST_ENTITY_TOO_LARGE;
+  }
+  case CoAP::CodeType::UNSUPPORTED_CONTENT_FORMAT: {
+    return ResponseCode::UNSUPPORTED_CONTENT_FORMAT;
+  }
+  default: { return ResponseCode::UNHANDLED; }
+  }
+}
+
+unique_ptr<Response> makeResponse(const CoAP::Message *message) {
+  unique_ptr<Response> response;
+  if (message->getHeader().getCodeType() == CoAP::CodeType::CONTENT) {
+    auto payload = message->getBody();
+    response = make_unique<Response>(
+        message->getReceiverIP(), message->getReceiverPort(),
+        message->getHeader().getMessageID(), message->getToken(),
+        MessageType::NOT_RECOGNIZED, ResponseCode::CONTENT, payload);
+  } else if (message->getHeader().getCodeType() == CoAP::CodeType::CONTINUE) {
+    // @TODO: handle segmented packets
+  } else if (message->getHeader().getCodeType() != CoAP::CodeType::UNHANDLED) {
+    response = make_unique<Response>(
+        message->getReceiverIP(), message->getReceiverPort(),
+        message->getHeader().getMessageID(), message->getToken(),
+        MessageType::NOT_RECOGNIZED,
+        convert(message->getHeader().getCodeType()));
+  }
+  return response;
+}
+
 CoAP_Decoder::CoAP_Decoder(
     shared_ptr<RegistrationInterface> registration,
     shared_ptr<ThreadsafeUniqueQueue<CoAP::Message>> message_buffer,
-    shared<ResponseHandler> response_handler)
+    shared_ptr<ResponseHandler> response_handler)
     : registration_(registration), message_buffer_(message_buffer),
       response_handler_(response_handler),
       logger_(LoggerRepository::getInstance().registerTypedLoger(this)) {}
 
 bool CoAP_Decoder::processIfResponse(const CoAP::Message *message) {
   if (message->getHeader().getMesageType() ==
-          CoAP::MessageType::ACKNOWLEDGMENT &&
-      message->getHeader().getCodeType() == CoAP::CodeType::CONTENT) {
-    auto options = message->getOptions();
-    for
+      CoAP::MessageType::ACKNOWLEDGMENT) {
+    auto response = makeResponse(message);
+    if (response) {
+      response_handler_->setResponse(move(response));
+      return true;
+    }
   }
   return false;
 }
