de359c90f14207ff54518b519e9516d3c924eb37
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2020-08-25 12:01:30 +0200

    remove encoding, add RawBytePack decoding

--- sources/CoAP/Payload_Decoder/PayloadDecoder.cpp
+++ sources/CoAP/Payload_Decoder/PayloadDecoder.cpp
@@ -1,6 +1,7 @@
 #include "PayloadDecoder.hpp"
 #include "CoRE_Link.hpp"
 #include "PlainText.hpp"
+#include "RawBytePack.hpp"
 
 using namespace std;
 
@@ -81,65 +82,11 @@ shared_ptr<PayloadFormat> decode(shared_ptr<ContentFormat> format,
     result = decodeJSON(payload);
     break;
   }
-  case ContentFormatType::UNRECOGNIZED:
+  case ContentFormatType::UNRECOGNIZED: {
+    result = make_shared<RawBytePack>(format->getAsShort(), payload);
+  }
   default: { break; }
-    return result;
   }
+  return result;
 }
-
-vector<uint8_t> encodePlainText(string payload) {
-  return vector<uint8_t>(payload.begin(), payload.end());
-}
-
-vector<uint8_t> encodeOpaque(string payload) {
-  throwCodingError("Opaque", false);
-}
-vector<uint8_t> encodeCBOR(string payload) { throwCodingError("CBOR", false); }
-vector<uint8_t> encodeSenMLJSON(string payload) {
-  throwCodingError("SehML JSON", false);
-}
-vector<uint8_t> encodeSenMLCBOR(string payload) {
-  throwCodingError("SenML CBOR", false);
-}
-vector<uint8_t> encodeTLV(string payload) { throwCodingError("TLV", false); }
-vector<uint8_t> encodeJSON(string payload) { throwCodingError("JSON", false); }
-
-vector<uint8_t> encode(shared_ptr<PayloadFormat> format) {
-  vector<uint8_t> result;
-  switch (format->getContentFormatType()) {
-  case ContentFormatType::PLAIN_TEXT:
-  case ContentFormatType::CORE_LINK: {
-    result = encodePlainText(format->toString());
-    break;
-  }
-  case ContentFormatType::OPAQUE: {
-    result = encodeOpaque(format->toString());
-    break;
-  }
-  case ContentFormatType::CBOR: {
-    result = encodeCBOR(format->toString());
-    break;
-  }
-  case ContentFormatType::SENML_JSON: {
-    result = encodeSenMLJSON(format->toString());
-    break;
-  }
-  case ContentFormatType::SENML_CBOR: {
-    result = encodeSenMLCBOR(format->toString());
-    break;
-  }
-  case ContentFormatType::TLV: {
-    result = encodeTLV(format->toString());
-    break;
-  }
-  case ContentFormatType::JSON: {
-    result = encodeJSON(format->toString());
-    break;
-  }
-  case ContentFormatType::UNRECOGNIZED:
-  default: { break; }
-    return result;
-  }
-}
-
 } // namespace CoAP
