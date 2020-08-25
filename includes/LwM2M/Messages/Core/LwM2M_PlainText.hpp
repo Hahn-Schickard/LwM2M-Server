c00480af6a7e653a01e9e689dfbcbb0efd761459
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2020-08-25 16:59:25 +0200

    mplement PlainText DataFormat

---
+++ includes/LwM2M/Messages/Core/LwM2M_PlainText.hpp
@@ -0,0 +1,21 @@
+#ifndef __LWM2M_PLAIN_TEXT_DATA_FORMAT_TYPE_HPP
+#define __LWM2M_PLAIN_TEXT_DATA_FORMAT_TYPE_HPP
+
+#include "LwM2M_DataFormat.hpp"
+
+namespace LwM2M {
+class PlainText : public DataFormat {
+  std::string value_;
+
+public:
+  PlainText();
+  PlainText(std::string value);
+  PlainText(std::vector<uint8_t> bytestream);
+
+  std::vector<uint8_t> getValue() override;
+  std::vector<uint8_t> getBytes() override;
+  std::string toString() override;
+};
+} // namespace LwM2M
+
+#endif //__LWM2M_PLAIN_TEXT_DATA_FORMAT_TYPE_HPP
