330f909f0b05838ac80fa8aac1b0d7556c8a8176
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2020-09-09 13:32:09 +0200

    refactor Observe_Request into ServerRequest_Observe
    Signed-off-by: Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>

--- includes/LwM2M/Messages/InformationReportingMessages/InformationReportingMessages.hpp
+++ includes/LwM2M/Messages/InformationReportingMessages/InformationReportingMessages.hpp
@@ -4,9 +4,9 @@
 #include "ClientMessage_Notify.hpp"
 #include "InformationReportingInterfaceMessage.hpp"
 #include "ObserveComposite_Request.hpp"
-#include "Observe_Request.hpp"
 #include "Send_Request.hpp"
 #include "ServerRequest_CancelObservation.hpp"
 #include "ServerRequest_CancelObserveComposite.hpp"
+#include "ServerRequest_Observe.hpp"
 
 #endif //__LWM2M_INFORMATION_REPOTRING_MESSAGES_HPP
