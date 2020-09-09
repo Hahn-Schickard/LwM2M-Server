8e6e763c59e39e8a0c887987f1bd7925bcdc9fb1
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2020-09-09 13:30:10 +0200

    refactor Notify_Request into ClientMessage_Notify

---
+++ includes/LwM2M/Messages/InformationReportingMessages/ClientMessage_Notify.hpp
@@ -0,0 +1,17 @@
+#ifndef __LWM2M_INFORMATION_REPORTING_INTERFACE_CLIENT_MESSAGE_NOTIFY_HPP
+#define __LWM2M_INFORMATION_REPORTING_INTERFACE_CLIENT_MESSAGE_NOTIFY_HPP
+
+#include "InformationReportingInterfaceMessage.hpp"
+
+namespace LwM2M {
+struct ClientMessage_Notify : public InformationReporting_Interface_Message {
+  std::string value_;
+
+  ClientMessage_Notify(std::string endpoint_address, unsigned int endpoint_port,
+                       uint16_t message_id, std::vector<uint8_t> token,
+                       std::string value);
+};
+
+} // namespace LwM2M
+
+#endif //__LWM2M_INFORMATION_REPORTING_INTERFACE_CLIENT_MESSAGE_NOTIFY_HPP
--- includes/LwM2M/Messages/InformationReportingMessages/InformationReportingMessages.hpp
+++ includes/LwM2M/Messages/InformationReportingMessages/InformationReportingMessages.hpp
@@ -1,8 +1,8 @@
 #ifndef __LWM2M_INFORMATION_REPOTRING_MESSAGES_HPP
 #define __LWM2M_INFORMATION_REPOTRING_MESSAGES_HPP
 
+#include "ClientMessage_Notify.hpp"
 #include "InformationReportingInterfaceMessage.hpp"
-#include "Notify_Request.hpp"
 #include "ObserveComposite_Request.hpp"
 #include "Observe_Request.hpp"
 #include "Send_Request.hpp"
