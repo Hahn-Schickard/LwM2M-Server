0b1c8d9108991be4c054be6a07a3a2c51501ff7d
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2021-02-09 14:06:25 +0100

    rework DeviceRegistry to use build devices instead of handling ClientRequests

--- includes/Device_Registry/DeviceRegistry.hpp
+++ includes/Device_Registry/DeviceRegistry.hpp
@@ -1,51 +1,96 @@
-#ifndef __LWM2M_MODEL_REGISTRATION_INTERFACE_HPP
-#define __LWM2M_MODEL_REGISTRATION_INTERFACE_HPP
+#ifndef __LWM2M_MODEL_DEVICE_REGISTRY_HPP
+#define __LWM2M_MODEL_DEVICE_REGISTRY_HPP
 
+#include "Device.hpp"
 #include "Event_Model.hpp"
 #include "Logger.hpp"
-#include "LwM2M_Device.hpp"
-#include "LwM2M_ObjectDescriptor.hpp"
-#include "Message_Encoder.hpp"
-#include "RegistrationMessages.hpp"
 #include "RegistryEvent.hpp"
-#include "Response_Handler.hpp"
 
+#include <cstdint>
 #include <memory>
+#include <stdexcept>
 #include <string>
 #include <unordered_map>
 
 namespace LwM2M {
 
-using ObjectDescriptorPair =
-    std::pair<std::shared_ptr<ObjectDescriptor>, std::vector<uint32_t>>;
+struct ObjectDescriptorNotSupported : std::runtime_error {
+  ObjectDescriptorNotSupported(uint32_t object_id);
+};
 
-using DevicePtr = std::shared_ptr<Device>;
+struct DeviceNotFound : std::runtime_error {
+  DeviceNotFound(std::string const &identifier);
+};
+
+using SupportedObjectDescripotrsMap =
+    std::unordered_map<uint32_t, ObjectDescriptorPtr>;
+using SupportedObjectDescripotrsMapPtr =
+    std::shared_ptr<SupportedObjectDescripotrsMap>;
 
 class DeviceRegistry : public Event_Model::EventSource<RegistryEvent> {
-  std::unordered_map<uint32_t, std::shared_ptr<ObjectDescriptor>>
-      supported_descriptors_;
-  std::unordered_map<std::string, std::shared_ptr<Device>> device_registery_;
-  std::shared_ptr<MessageEncoder> encoder_;
+  using DeviceRegistryMap = std::unordered_map<std::string, DevicePtr>;
+
+  SupportedObjectDescripotrsMap supported_descriptors_;
+  DeviceRegistryMap device_registery_;
   std::shared_ptr<HaSLL::Logger> logger_;
 
-  bool isRegistered(std::string device_id);
-
-  std::unordered_map<uint32_t, ObjectDescriptorPair> assignObjectInstances(
-      std::unordered_map<unsigned int, std::vector<unsigned int>> objects);
-
 public:
-  DeviceRegistry(std::shared_ptr<MessageEncoder> encoder,
-                 const std::string &configuration_path);
+  DeviceRegistry(const std::string &configuration_path);
 
+  // Maybe rework into check if ElementID is supported?
+  SupportedObjectDescripotrsMapPtr getSupportedDescriptors();
+
+  bool isRegistered(std::string identifier);
+
+  /**
+   * @brief Registers a given LwM2M::Device to the current registry. If a given
+   * LwM2M::Device already exists in the registry, calls deregisterDevice()
+   * first. Informs all registered Event_Model::EventListener<RegistryEvent>
+   * instances that a new LwM2M::Device has been added to the registry.
+   *
+   * @param new_device
+   */
+  void registerDevice(DevicePtr new_device);
+
+  /**
+   * @brief Check if a given LwM2M::Device exists in the current registry and
+   * updates it with the given value. Informs all registered
+   * Event_Model::EventListener<RegistryEvent> instances that a given
+   * LwM2M::Device has been updated.
+   *
+   * @throw DeviceNotFound - if a given LwM2M::Device does not exist in the
+   * current registry.
+   *
+   * @param updated_device
+   */
+  void updateDevice(DevicePtr updated_device);
+
+  /**
+   * @brief Checks if the registry contains a LwM2M::Device with a given
+   * identifier and removes it from the current registry. Informs all registered
+   * Event_Model::EventListener<RegistryEvent> instances that a given
+   * LwM2M::Device has been removed.
+   *
+   * @throw DeviceNotFound - if a given LwM2M::Device does not exist in the
+   * current registry.
+   *
+   * @param identifier
+   */
+  void deregisterDevice(std::string identifier);
+
+  /**
+   * @brief Check if the registry contains a LwM2M::Device with a given
+   * identifier and returns a std::shared_ptr of it to the caller.
+   *
+   * @throw DeviceNotFound - if a given LwM2M::Device does not exist in the
+   * current registry.
+   *
+   * @param identifier
+   * @return DevicePtr
+   */
   DevicePtr getDevice(std::string identifier);
-
-  bool handleRequest(std::unique_ptr<ClientRequest_Register> request);
-
-  bool handleRequest(std::unique_ptr<ClientRequest_Update> request);
-
-  bool handleRequest(std::unique_ptr<ClientRequest_Deregister> request);
 };
 
 } // namespace LwM2M
 
-#endif //__LWM2M_MODEL_REGISTRATION_INTERFACE_HPP
+#endif //__LWM2M_MODEL_DEVICE_REGISTRY_HPP
--- sources/Device_Registry/DeviceRegistry.cpp
+++ sources/Device_Registry/DeviceRegistry.cpp
@@ -1,23 +1,21 @@
 #include "DeviceRegistry.hpp"
 #include "LoggerRepository.hpp"
-#include "StringSpliter.hpp"
 #include "XmlParser.hpp"
-
-#include <stdexcept>
 
 using namespace std;
 using namespace HaSLL;
 
 namespace LwM2M {
+ObjectDescriptorNotSupported::ObjectDescriptorNotSupported(uint32_t object_id)
+    : runtime_error("Object with id: " + to_string(object_id) +
+                    " is not supported by the current registry.") {}
 
-struct ObjectDescriptorNotSupported : runtime_error {
-  ObjectDescriptorNotSupported(string const &message)
-      : runtime_error(message) {}
-};
+DeviceNotFound::DeviceNotFound(string const &identifier)
+    : runtime_error("Device with id: " + identifier +
+                    " could not be found in the registry.") {}
 
-DeviceRegistry::DeviceRegistry(shared_ptr<MessageEncoder> encoder,
-                               const string &configuration_path)
-    : Event_Model::EventSource<RegistryEvent>(), encoder_(encoder),
+DeviceRegistry::DeviceRegistry(const string &configuration_path)
+    : Event_Model::EventSource<RegistryEvent>(),
       logger_(LoggerRepository::getInstance().registerTypedLoger(this)) {
   try {
     supported_descriptors_ = deserializeModel(configuration_path);
@@ -28,28 +26,57 @@ DeviceRegistry::DeviceRegistry(shared_ptr<MessageEncoder> encoder,
   }
 }
 
-unordered_map<uint32_t, ObjectDescriptorPair>
-DeviceRegistry::assignObjectInstances(
-    unordered_map<unsigned int, vector<unsigned int>> objects) {
-  unordered_map<uint32_t, ObjectDescriptorPair> result;
-  for (auto &object : objects) {
-    auto object_id = object.first;
-    auto instance_ids = object.second;
-    auto it = supported_descriptors_.find(object_id);
-    if (it != supported_descriptors_.end()) {
-      result.emplace(object_id, ObjectDescriptorPair(it->second, instance_ids));
-    } else {
-      string error_msg = "Object ID " + to_string(object_id) +
-                         " is not supported by the server.";
-      throw ObjectDescriptorNotSupported(move(error_msg));
-    }
-  }
-  return result;
+SupportedObjectDescripotrsMapPtr DeviceRegistry::getSupportedDescriptors() {
+  return make_shared<SupportedObjectDescripotrsMap>(supported_descriptors_);
 }
 
-bool DeviceRegistry::isRegistered(string device_id) {
-  return (device_registery_.find(device_id) != device_registery_.end()) ? true
-                                                                        : false;
+bool DeviceRegistry::isRegistered(std::string identifier) {
+  return (device_registery_.find(identifier) != device_registery_.end())
+             ? true
+             : false;
+}
+
+void DeviceRegistry::registerDevice(DevicePtr new_device) {
+  if (isRegistered(new_device->getDeviceId())) {
+    logger_->log(SeverityLevel::TRACE,
+                 "Device {} with id {} already exists in the registry!",
+                 new_device->getName(), new_device->getDeviceId());
+    deregisterDevice(new_device->getDeviceId());
+  }
+  device_registery_.emplace(new_device->getDeviceId(), new_device);
+  logger_->log(SeverityLevel::TRACE,
+               "Device {} with id {} has been registered.",
+               new_device->getName(), new_device->getDeviceId());
+  notify(make_shared<RegistryEvent>(RegistryEvent{
+      RegistryEventType::REGISTERED, new_device->getDeviceId(), new_device}));
+}
+
+void DeviceRegistry::updateDevice(DevicePtr updated_device) {
+  auto it = device_registery_.find(updated_device->getDeviceId());
+  if (it != device_registery_.end()) {
+    it->second = updated_device;
+    logger_->log(SeverityLevel::TRACE, "Device {} with id {} has been updated.",
+                 updated_device->getName(), updated_device->getDeviceId());
+    notify(make_shared<RegistryEvent>(
+        RegistryEvent{RegistryEventType::UPDATED, updated_device->getDeviceId(),
+                      updated_device}));
+  } else {
+    throw DeviceNotFound(updated_device->getDeviceId());
+  }
+}
+
+void DeviceRegistry::deregisterDevice(std::string identifier) {
+  auto it = device_registery_.find(identifier);
+  if (it != device_registery_.end()) {
+    device_registery_.erase(it);
+    logger_->log(SeverityLevel::TRACE,
+                 "Device with id {} has been removed from the registry.",
+                 identifier);
+    notify(make_shared<RegistryEvent>(RegistryEvent{
+        RegistryEventType::DEREGISTERED, identifier, DevicePtr()}));
+  } else {
+    throw DeviceNotFound(identifier);
+  }
 }
 
 DevicePtr DeviceRegistry::getDevice(std::string identifier) {
@@ -57,113 +84,7 @@ DevicePtr DeviceRegistry::getDevice(std::string identifier) {
   if (it != device_registery_.end()) {
     return it->second;
   } else {
-    return DevicePtr();
+    throw DeviceNotFound(identifier);
   }
-}
-
-bool DeviceRegistry::handleRequest(unique_ptr<ClientRequest_Register> request) {
-  unique_ptr<ServerResponse_Register> result;
-  shared_ptr<RegistryEvent> event;
-  try {
-    auto object_instances =
-        assignObjectInstances(request->object_instances_map_);
-    auto new_device = make_shared<Device>(
-        encoder_, request->endpoint_name_, request->endpoint_address_,
-        request->endpoint_port_, request->life_time_, request->version_,
-        request->binding_, request->queue_mode_, request->sms_number_,
-        object_instances);
-    if (isRegistered(new_device->getDeviceId()))
-      device_registery_.erase(
-          device_registery_.find(new_device->getDeviceId()));
-    device_registery_.emplace(new_device->getDeviceId(), new_device);
-    event = make_shared<RegistryEvent>(RegistryEvent{
-        RegistryEventType::REGISTERED, new_device->getDeviceId(), new_device});
-    result = make_unique<ServerResponse_Register>(
-        request->endpoint_address_, request->endpoint_port_,
-        request->message_id_.value(), request->token_, MessageType::REGISTER,
-        ResponseCode::CREATED, vector<string>{"rd", new_device->getDeviceId()});
-  } catch (ObjectDescriptorNotSupported &ex) {
-    logger_->log(SeverityLevel::ERROR, ex.what());
-    result = make_unique<ServerResponse_Register>(
-        request->endpoint_address_, request->endpoint_port_,
-        request->message_id_.value(), request->token_, MessageType::REGISTER,
-        ResponseCode::BAD_REQUEST);
-  }
-  encoder_->encode(move(result));
-  if (event)
-    notify(move(event));
-  return true;
-}
-
-bool DeviceRegistry::handleRequest(unique_ptr<ClientRequest_Update> request) {
-  try {
-    auto device = device_registery_.at(request->location_);
-    bool updated = false;
-    if (device) {
-      if (request->binding_) {
-        device->updateBinding(request->binding_.value());
-        updated = true;
-      }
-      if (request->lifetime_) {
-        device->updateLifetime(request->lifetime_.value());
-        updated = true;
-      }
-      if (request->sms_number_) {
-        device->updateSMS_Number(request->sms_number_.value());
-        updated = true;
-      }
-      if (!request->object_instances_map_.empty()) {
-        auto object_instances =
-            assignObjectInstances(request->object_instances_map_);
-        device->updateObjectsMap(object_instances);
-        updated = true;
-      }
-      if (updated)
-        notify(make_shared<RegistryEvent>(RegistryEvent{
-            RegistryEventType::UPDATED, request->location_, device}));
-      encoder_->encode(make_unique<Response>(
-          request->endpoint_address_, request->endpoint_port_,
-          request->message_id_.value(), request->token_, MessageType::UPDATE,
-          ResponseCode::CHANGED));
-    } else
-      encoder_->encode(make_unique<Response>(
-          request->endpoint_address_, request->endpoint_port_,
-          request->message_id_.value(), request->token_, MessageType::UPDATE,
-          ResponseCode::NOT_FOUND));
-  } catch (ObjectDescriptorNotSupported &ex) {
-    logger_->log(SeverityLevel::ERROR, ex.what());
-    encoder_->encode(make_unique<ServerResponse_Register>(
-        request->endpoint_address_, request->endpoint_port_,
-        request->message_id_.value(), request->token_, MessageType::UPDATE,
-        ResponseCode::BAD_REQUEST));
-  }
-  return true;
-}
-
-bool DeviceRegistry::handleRequest(
-    unique_ptr<ClientRequest_Deregister> request) {
-  try {
-    if (isRegistered(request->location_)) {
-      device_registery_.erase(device_registery_.find(request->location_));
-      notify(make_shared<RegistryEvent>(
-          RegistryEvent{RegistryEventType::DEREGISTERED, request->location_,
-                        shared_ptr<Device>()}));
-      encoder_->encode(make_unique<Response>(
-          request->endpoint_address_, request->endpoint_port_,
-          request->message_id_.value(), request->token_,
-          MessageType::DEREGISTER, ResponseCode::DELETED));
-    } else
-      encoder_->encode(make_unique<Response>(
-          request->endpoint_address_, request->endpoint_port_,
-          request->message_id_.value(), request->token_,
-          MessageType::DEREGISTER, ResponseCode::NOT_FOUND));
-  } catch (ObjectDescriptorNotSupported &ex) {
-    logger_->log(SeverityLevel::ERROR, ex.what());
-    encoder_->encode(make_unique<ServerResponse_Register>(
-        request->endpoint_address_, request->endpoint_port_,
-        request->message_id_.value(), request->token_, MessageType::DEREGISTER,
-        ResponseCode::BAD_REQUEST));
-  }
-  return true;
 }
 } // namespace LwM2M
