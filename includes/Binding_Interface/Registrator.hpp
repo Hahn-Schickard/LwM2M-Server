41c043e07f84ce1284b9a982ea272b3d76455bfa
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2021-02-10 14:30:00 +0100

    implement Registrator interface

--- includes/Binding_Interface/Registrator.hpp
+++ includes/Binding_Interface/Registrator.hpp
@@ -1,8 +1,30 @@
 #ifndef __LWM2M_BINDING_REGISTRATOR_INTERFACE_HPP
 #define __LWM2M_BINDING_REGISTRATOR_INTERFACE_HPP
 
+#include "Deregister.hpp"
+#include "DeviceRegistry.hpp"
+#include "Register.hpp"
 #include "Requester.hpp"
+#include "Update.hpp"
 
-namespace LwM2M {}
+namespace LwM2M {
+
+class Registrator {
+  DeviceRegistryPtr registry_;
+  RequesterPtr requester_;
+
+  ObjectDescriptorsMap assignObjectDescriptors(
+      const std::unordered_map<unsigned int, std::vector<unsigned int>>
+          requested_object_instances);
+
+public:
+  Registrator(DeviceRegistryPtr registry, RequesterPtr requester);
+
+  RegisterResponsePtr handleRquest(RegisterRequestPtr request);
+  UpdateResponsePtr handleRquest(UpdateRequestPtr request);
+  DeregisterResponsePtr handleRquest(DeregisterRequestPtr request);
+};
+
+} // namespace LwM2M
 
 #endif //__LWM2M_BINDING_REGISTRATOR_INTERFACE_HPP
---
+++ sources/Binding_Interface/Registrator.cpp
@@ -0,0 +1,85 @@
+#include "Registrator.hpp"
+
+using namespace std;
+
+namespace LwM2M {
+
+ObjectDescriptorsMap Registrator::assignObjectDescriptors(
+    const unordered_map<unsigned int, vector<unsigned int>>
+        requested_object_instances) {
+  auto supported_object_descriptors = registry_->getSupportedDescriptors();
+  ObjectDescriptorsMap result;
+  for (auto requested_instance : requested_object_instances) {
+    auto descriptor =
+        supported_object_descriptors->find(requested_instance.first);
+    if (descriptor != supported_object_descriptors->end()) {
+      ObjectDescriptorPair instace_descriptor_pair =
+          make_pair(descriptor->second, requested_instance.second);
+      result.emplace(descriptor->first, move(instace_descriptor_pair));
+    }
+  }
+  return result;
+}
+
+Registrator::Registrator(DeviceRegistryPtr registry, RequesterPtr requester)
+    : registry_(registry), requester_(requester) {
+  if (!registry_) {
+    throw invalid_argument("Device Registry can not be a nullptr.");
+  }
+  if (!requester_) {
+    throw invalid_argument("Requester can not be a nullptr.");
+  }
+}
+
+RegisterResponsePtr Registrator::handleRquest(RegisterRequestPtr request) {
+  auto object_instances =
+      assignObjectDescriptors(request->object_instances_map_);
+  try {
+    auto device = make_shared<Device>(
+        requester_, request->endpoint_, object_instances, request->life_time_,
+        request->endpoint_name_, request->version_, request->binding_,
+        request->queue_mode_);
+    auto location = registry_->registerDevice(device);
+    return request->makeResponse(ResponseCode::CREATED, location);
+  } catch (exception &ex) {
+    // TODO: log exception here?
+    return request->makeResponse(ResponseCode::BAD_REQUEST);
+  }
+}
+
+UpdateResponsePtr Registrator::handleRquest(UpdateRequestPtr request) {
+  auto device = registry_->getDevice(request->location_);
+  if (device) {
+    if (!request->object_instances_map_.empty()) {
+      auto object_instances =
+          assignObjectDescriptors(request->object_instances_map_);
+      device->updateObjectsMap(object_instances);
+    }
+    if (request->lifetime_.has_value()) {
+      device->updateLifetime(request->lifetime_.value());
+    }
+    if (request->binding_.has_value()) {
+      device->updateBinding(request->binding_.value());
+    }
+    if (request->sms_number_.has_value()) {
+      // TODO: implement endpoint updating
+      return request->makeResponse();
+    }
+    registry_->updateDevice(device);
+    return request->makeResponse(ResponseCode::CHANGED);
+  } else {
+    return request->makeResponse(ResponseCode::NOT_FOUND);
+  }
+}
+
+DeregisterResponsePtr Registrator::handleRquest(DeregisterRequestPtr request) {
+  try {
+    registry_->deregisterDevice(request->location_);
+    return request->makeResponse(ResponseCode::DELETED);
+  } catch (DeviceNotFound &ex) {
+    return request->makeResponse(ResponseCode::NOT_FOUND);
+  } catch (exception &ex) {
+    return request->makeResponse();
+  }
+}
+} // namespace LwM2M
