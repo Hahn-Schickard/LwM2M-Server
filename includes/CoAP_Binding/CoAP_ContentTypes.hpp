88c9cf4712ac9a16ec5d5eeb95667151bee0052c
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2021-02-22 10:19:41 +0100

    implement TLV decoder/encoder pair for CoAP binding

--- includes/CoAP_Binding/CoAP_ContentTypes.hpp
+++ includes/CoAP_Binding/CoAP_ContentTypes.hpp
@@ -1,7 +1,9 @@
 #ifndef __LWM2M_COAP_CONTENT_FORMAT_TYPES_HPP
 #define __LWM2M_COAP_CONTENT_FORMAT_TYPES_HPP
 
+#include "CoAP/ContentFormatType.hpp"
 #include "CoAP/SupportedContentFormats.hpp"
+#include "TLV.hpp"
 
 namespace CoAP {
 namespace ContentFormatEncodings {
@@ -14,6 +16,9 @@ typedef ContentFormatEncoding<11542, decltype("LwM2M TLV"_name)> LwM2M_TLV;
 typedef ContentFormatEncoding<11543, decltype("LwM2M CBOR"_name)> LwM2M_CBOR;
 typedef ContentFormatEncoding<11544, decltype("LwM2M JSON"_name)> LwM2M_JSON;
 } // namespace ContentFormatEncodings
+
+template <> LwM2M::TLV_Pack decode(PayloadPtr payload);
+template <> PayloadPtr encode(LwM2M::TLV_Pack content);
 } // namespace CoAP
 
 #endif //__LWM2M_COAP_CONTENT_FORMAT_TYPES_HPP
---
+++ sources/CoAP_Binding/CoAP_ContentTypes.cpp
@@ -0,0 +1,23 @@
+#include "CoAP_ContentTypes.hpp"
+
+using namespace std;
+
+namespace CoAP {
+
+template <> LwM2M::TLV_Pack decode(PayloadPtr payload) {
+  if (payload->getContentFormat()->getIndex() !=
+      ContentFormatEncodings::LwM2M_TLV::index) {
+    throw WrongContentFormatTypeDecoder(
+        string(ContentFormatEncodings::LwM2M_TLV::toString()),
+        payload->getContentFormat());
+  }
+  auto buffer = payload->getBytes();
+
+  return LwM2M::TLV_Pack(buffer);
+}
+
+template <> PayloadPtr encode(LwM2M::TLV_Pack content) {
+  return make_shared<Payload>(ContentFormatEncodings::LwM2M_TLV::index,
+                              content.getBytes());
+}
+} // namespace CoAP
