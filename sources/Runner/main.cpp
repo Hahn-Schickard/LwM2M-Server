89d130fec02053bb6fbcd9d6324d78b65387649c
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2021-02-22 12:03:35 +0100

    add debug print for contet format, remove unused device event print outs

--- sources/Runner/main.cpp
+++ sources/Runner/main.cpp
@@ -1,10 +1,12 @@
 #include "CoAP_Binding.hpp"
+#include "CoAP_ContentTypes.hpp"
 #include "Event_Model.hpp"
 #include "LoggerRepository.hpp"
 #include "RegistryEvent.hpp"
 
 #include <chrono>
 #include <iostream>
+#include <locale>
 #include <thread>
 
 using namespace HaSLL;
@@ -22,10 +24,6 @@ public:
       cout << "A new device with id: " << event->identifier_
            << " has been registered!" << endl;
       auto device = event->device_;
-      cout << "Sent a write request to Device object 1 instance 0, resource 1"
-           << endl;
-      cout << "Sent a read request to Device: " << device->getDeviceId()
-           << endl;
       break;
     }
     case RegistryEventType::UPDATED: {
@@ -46,6 +44,36 @@ public:
   }
 };
 
+string isSupported(uint16_t index) {
+  return get<0>(CoAP::SupportedContentFormats::getSupportedContentFormat(
+             index)) == true
+             ? "is supported."
+             : "is NOT supported.";
+}
+
+void printSupportedContentFormats() {
+  cout << "Content Format: " << CoAP::ContentFormatEncodings::PlainText::index
+       << " " << CoAP::ContentFormatEncodings::PlainText::toString() << " "
+       << isSupported(CoAP::ContentFormatEncodings::PlainText::index) << endl;
+  cout << "Content Format: " << CoAP::ContentFormatEncodings::CoRE_Link::index
+       << " " << CoAP::ContentFormatEncodings::CoRE_Link::toString() << " "
+       << isSupported(CoAP::ContentFormatEncodings::CoRE_Link::index) << endl;
+  cout << "Content Format: "
+       << CoAP::ContentFormatEncodings::Octet_Stream::index << " "
+       << CoAP::ContentFormatEncodings::Octet_Stream::toString() << " "
+       << isSupported(CoAP::ContentFormatEncodings::Octet_Stream::index)
+       << endl;
+  cout << "Content Format: " << CoAP::ContentFormatEncodings::LwM2M_TLV::index
+       << " " << CoAP::ContentFormatEncodings::LwM2M_TLV::toString() << " "
+       << isSupported(CoAP::ContentFormatEncodings::LwM2M_TLV::index) << endl;
+  cout << "Content Format: " << CoAP::ContentFormatEncodings::LwM2M_CBOR::index
+       << " " << CoAP::ContentFormatEncodings::LwM2M_CBOR::toString() << " "
+       << isSupported(CoAP::ContentFormatEncodings::LwM2M_CBOR::index) << endl;
+  cout << "Content Format: " << CoAP::ContentFormatEncodings::LwM2M_JSON::index
+       << " " << CoAP::ContentFormatEncodings::LwM2M_JSON::toString() << " "
+       << isSupported(CoAP::ContentFormatEncodings::LwM2M_JSON::index) << endl;
+}
+
 int main(int argc, const char *argv[]) {
   try {
     LoggerRepository::initialise("loggerConfig.json");
@@ -54,10 +82,10 @@ int main(int argc, const char *argv[]) {
     LoggerRepository::getInstance().configure(SeverityLevel::TRACE);
 
     auto coap_binding = make_unique<CoAP_Binding>(
-        make_shared<CoAP_BindingConfig>(),
         make_shared<DeviceRegistry>("model/descriptors.xml"));
     auto registration_listener =
         make_unique<RegistrationListener>(coap_binding->getEventSource());
+    printSupportedContentFormats();
     try {
       coap_binding->start();
       logger->log(SeverityLevel::INFO, "Started LwM2M Server!");
