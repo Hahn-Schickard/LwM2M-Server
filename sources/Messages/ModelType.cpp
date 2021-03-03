aa362087689d70d89536927ded7c79c83e14bd00
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2021-03-03 11:44:51 +0100

    add to string converters for ModelType header

---
+++ sources/Messages/ModelType.cpp
@@ -0,0 +1,35 @@
+#include "ModelType.hpp"
+
+using namespace std;
+
+namespace LwM2M {
+string toString(LwM2M_Version version) {
+  switch (version) {
+  case LwM2M_Version::V1_0: {
+    return "1.0";
+  }
+  case LwM2M_Version::V1_1: {
+    return "1.1.";
+  }
+  default: { return "Unrecognized"; }
+  }
+}
+
+string toString(BindingType binding) {
+  switch (binding) {
+  case BindingType::UDP: {
+    return "UDP";
+  }
+  case BindingType::TCP: {
+    return "TCP";
+  }
+  case BindingType::SMS: {
+    return "SMS";
+  }
+  case BindingType::NON_IP: {
+    return "Non IP";
+  }
+  default: { return "Malformated"; }
+  }
+}
+} // namespace LwM2M
