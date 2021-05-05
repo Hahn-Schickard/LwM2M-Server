394b75c7bafd63a540141a328e009414368bf471
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2021-05-05 12:42:36 +0200

    repeat test cases 20 times and throw on failure to stop the test from further execution

--- unit_tests/testRunner.cpp
+++ unit_tests/testRunner.cpp
@@ -6,6 +6,8 @@ int main(int argc, char **argv) {
       "./log", "tests.log", "[%Y-%m-%d-%H:%M:%S:%F %z][%n]%^[%l]: %v%$",
       HaSLL::SeverityLevel::TRACE, false, 8192, 2, 25, 100, 1);
   HaSLL::LoggerRepository::initialise(config);
+  testing::GTEST_FLAG(repeat) = 20;
+  testing::GTEST_FLAG(throw_on_failure) = true;
 
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
