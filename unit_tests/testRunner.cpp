6a2a9d47eebfb6dfe23f2d979bf8601c951a1336
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2021-05-06 14:07:01 +0200

    remove test repeats

--- unit_tests/testRunner.cpp
+++ unit_tests/testRunner.cpp
@@ -6,7 +6,6 @@ int main(int argc, char **argv) {
       "./log", "tests.log", "[%Y-%m-%d-%H:%M:%S:%F %z][%n]%^[%l]: %v%$",
       HaSLL::SeverityLevel::TRACE, false, 8192, 2, 25, 100, 1);
   HaSLL::LoggerRepository::initialise(config);
-  testing::GTEST_FLAG(repeat) = 20;
   testing::GTEST_FLAG(throw_on_failure) = true;
 
   testing::InitGoogleTest(&argc, argv);
