e4a078243a3e1319b75db0a60f5add3e926ee214
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2021-08-02 14:08:13 +0200

    move CoAP_Server unit into CoAP_Binding

--- includes/CoAP_Binding/CoAP_Binding.hpp
+++ includes/CoAP_Binding/CoAP_Binding.hpp
@@ -2,20 +2,43 @@
 #define __LWM2M_COAP_BINDING_HPP
 
 #include "Binding.hpp"
-
-#include "CoAP_Server.hpp"
+#include "CoAP/Socket.hpp"
+#include "CoAP_Config.hpp"
+#include "Logger.hpp"
+#include "Registrator.hpp"
 
 namespace LwM2M {
 
-class CoAP_Binding : public BindingInterface {
-  CoAP_ServerPtr server_;
-
-public:
+struct CoAP_Binding : public BindingInterface,
+                      public Registrator,
+                      public CoAP::Socket {
   CoAP_Binding(DeviceRegistryPtr registry, const std::string &config_filepath);
   CoAP_Binding(DeviceRegistryPtr registry, const CoAP_Config &config);
 
-  void start() override final;
-  void stop() override final;
+  ~CoAP_Binding();
+
+  // Requester interface implementation
+  std::future<DataFormatPtr>
+  requestData(ServerRequestPtr message) override final;
+  std::future<TargetContentVector>
+  requestMultiTargetData(ServerRequestPtr message) override final;
+  std::future<bool> requestAction(ServerRequestPtr message) override final;
+  std::future<ClientResponsePtr>
+  request(ServerRequestPtr message) override final;
+
+  void start();
+  void stop();
+
+private:
+  ClientResponsePtr makeClientResponse(CoAP::MessagePtr message);
+  void handleNotification(CoAP::MessagePtr message);
+  ServerResponsePtr handleRegistrationRequest(CoAP::MessagePtr message);
+  ServerResponsePtr handleRequest(CoAP::MessagePtr message);
+
+  // Socket interface implementation
+  void handleReceived(CoAP::MessagePtr message) override final;
+
+  std::shared_ptr<HaSLL::Logger> logger_;
 };
 } // namespace LwM2M
 
