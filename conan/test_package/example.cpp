21a494cf385dc2baae151c1fc4f35d0e4b4f492d
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2021-10-05 11:53:18 +0200

    rework server to unique_ptr for integratio n test and destroy it before exiting the test

--- conan/test_package/example.cpp
+++ conan/test_package/example.cpp
@@ -9,27 +9,32 @@ using namespace HaSLL;
 using namespace std;
 
 int main() {
-  auto config = HaSLL::Configuration(
-      "./log", "logfile.log", "[%Y-%m-%d-%H:%M:%S:%F %z][%n]%^[%l]: %v%$",
-      HaSLL::SeverityLevel::TRACE, false, 8192, 2, 25, 100, 1);
-  LoggerRepository::initialise(config);
-  auto logger = LoggerRepository::getInstance().registerLoger("Example_Runner");
-  LoggerRepository::getInstance().configure(SeverityLevel::TRACE);
+  {
+    auto config = HaSLL::Configuration(
+        "./log", "logfile.log", "[%Y-%m-%d-%H:%M:%S:%F %z][%n]%^[%l]: %v%$",
+        HaSLL::SeverityLevel::TRACE, false, 8192, 2, 25, 100, 1);
+    LoggerRepository::initialise(config);
+    auto logger =
+        LoggerRepository::getInstance().registerLoger("Integration_Runner");
+    LoggerRepository::getInstance().configure(SeverityLevel::TRACE);
 
-  LwM2M::Server server;
-  try {
-    server = LwM2M::Server("config/serverConfig.json");
-    server.start();
-    logger->log(SeverityLevel::INFO, "Started LwM2M Server!");
-    this_thread::sleep_for(1s);
+    unique_ptr<LwM2M::Server> server;
+    try {
+      server = make_unique<LwM2M::Server>("config/serverConfig.json");
+      server->start();
+      logger->log(SeverityLevel::INFO, "Started LwM2M Server!");
+      this_thread::sleep_for(1s);
 
-  } catch (exception &e) {
-    logger->log(SeverityLevel::ERROR, "Received an exception: {}", e.what());
-    cerr << e.what();
+    } catch (exception &e) {
+      logger->log(SeverityLevel::ERROR, "Received an exception: {}", e.what());
+      cerr << e.what();
+    }
+
+    if (server) {
+      server->stop();
+      logger->log(SeverityLevel::INFO, "Stopped LwM2M Server!");
+    }
   }
-
-  server.stop();
-  logger->log(SeverityLevel::INFO, "Stopped LwM2M Server!");
 
   exit(EXIT_SUCCESS);
 }
