b7d8cf11e704f056c126eb0dfcf5ec482eb41fc6
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2022-03-24 15:56:23 +0100

    implement handleData method, rework handleTargetContent and handleTargetContentVector to accept ObservedDataTypes map

--- sources/Runner/Observer.cpp
+++ sources/Runner/Observer.cpp
@@ -1,8 +1,11 @@
 
 #include "Observer.hpp"
 #include "DataFormat.hpp"
+
+#include "HSCUL/String.hpp"
 #include "Variant_Visitor.hpp"
 
+#include <iomanip>
 #include <iostream>
 
 using namespace std;
@@ -10,18 +13,73 @@ using namespace std;
 namespace LwM2M_Example {
 Observer::Observer(ObservablePtr element) : ObserverInterface(element) {}
 
-void handleData(DataFormatPtr data) {}
-
-void handleTargetContent(TargetContent data) {
-  cout << data.first.toString() << " ";
-  handleData(data.second);
+void handleData(DataFormatPtr data, DataType data_type) {
+  switch (data_type) {
+  case DataType::STRING: {
+    cout << data->get<string>();
+    break;
+  }
+  case DataType::SIGNED_INTEGER: {
+    cout << data->get<int64_t>();
+    break;
+  }
+  case DataType::UNSIGNED_INTEGER: {
+    cout << data->get<uint64_t>();
+    break;
+  }
+  case DataType::FLOAT: {
+    cout << data->get<double>();
+    break;
+  }
+  case DataType::BOOLEAN: {
+    cout << std::boolalpha << data->get<bool>();
+    break;
+  }
+  case DataType::OPAQUE: {
+    cout << HSCUL::hexify(data->get<std::vector<uint8_t>>());
+    break;
+  }
+  case DataType::TIME: {
+    cout << data->get<TimeStamp>().toString();
+    break;
+  }
+  case DataType::OBJECT_LINK: {
+    cout << data->get<ObjectLink>().toString();
+    break;
+  }
+  case DataType::CORE_LINK: {
+    cout << "CoRE Link format is not supported";
+    break;
+  }
+  case DataType::NONE: {
+    cout << "void";
+    break;
+  }
+  default: {
+    cout << "Could not resolve data type of given data element!";
+    break;
+  }
+  }
 }
 
-void handleTargetContentVector(TargetContentVector vector) {
+void handleTargetContent(TargetContent data,
+                         Observable::ObservedDataTypes observed_data_typese) {
+  cout << data.first.toString() << " ";
+  auto data_type = observed_data_typese.find(data.first);
+  if (data_type != observed_data_typese.end()) {
+    handleData(data.second, data_type->second);
+  } else {
+    cout << " has no observed data value";
+  }
+}
+
+void handleTargetContentVector(
+    TargetContentVector vector,
+    Observable::ObservedDataTypes observed_data_types) {
   cout << "{";
   for (auto target_data = vector.begin(); target_data != vector.end();
        ++target_data) {
-    handleTargetContent(*target_data);
+    handleTargetContent(*target_data, observed_data_types);
     if (next(target_data) != vector.end()) {
       cout << ",";
     }
@@ -61,17 +119,18 @@ void handleTargetAttributes(TargetAttributes attributes) {
 
 void Observer::handleEvent(PayloadDataPtr payload) {
   if (payload) {
+    cout << "Received: ";
     match(*payload,
           [&](DataFormatPtr data) {
-            handleData(data);
+            handleData(data, getObservedDataTypes().begin()->second);
             cout << endl;
           },
           [&](TargetContent data) {
-            handleTargetContent(data);
+            handleTargetContent(data, getObservedDataTypes());
             cout << endl;
           },
           [&](TargetContentVector data) {
-            handleTargetContentVector(data);
+            handleTargetContentVector(data, getObservedDataTypes());
             cout << endl;
           },
           [&](ElementID id) {
