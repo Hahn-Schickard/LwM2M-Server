143c54b81289dd45c372ee4203a5b36052aeedf3
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2022-07-12 12:54:26 +0200

    Refactor Writable into a Resource Trait

--- includes/Device_Model/Writable.hpp
+++ includes/Device_Model/Writable.hpp
@@ -2,31 +2,19 @@
 #define __LWM2M_WRITABLE_RESOURCE_HPP
 
 #include "CallableEntity.hpp"
-#include "Resource.hpp"
-#include "Write.hpp"
+#include "ResourceDescriptor.hpp"
 
 namespace LwM2M {
 
-template <typename T>
-class Writable : public Resource<T>, public CallableEntity {
+class Writable : public CallableEntity {
 public:
   Writable(ResourceDescriptorPtr descriptor, RequesterPtr requester,
-      EndpointPtr endpoint, ElementID id)
-      : Resource<T>(descriptor), CallableEntity(requester, endpoint, id) {}
+      EndpointPtr endpoint, ElementID id);
 
-  std::future<bool> write(DataVariant data) override {
-    auto payload = std::make_shared<DataFormat>(data);
-    auto message = std::make_shared<WriteRequest>(endpoint_, id_, payload);
-
-    return requester_->requestAction(message);
-  }
-
-  std::future<bool> write(T value) override {
-    return write(DataVariant(value));
-  }
+  std::future<bool> write(DataVariant data);
 };
 
-template <typename T> using WritablePtr = std::shared_ptr<Writable<T>>;
+using WritablePtr = std::shared_ptr<Writable>;
 
 } // namespace LwM2M
 
---
+++ sources/Device_Model/Writable.cpp
@@ -0,0 +1,17 @@
+#include "Writable.hpp"
+#include "Write.hpp"
+
+using namespace std;
+
+namespace LwM2M {
+Writable::Writable(ResourceDescriptorPtr descriptor, RequesterPtr requester,
+    EndpointPtr endpoint, ElementID id)
+    : CallableEntity(requester, endpoint, id) {}
+
+future<bool> Writable::write(DataVariant data) {
+  auto payload = make_shared<DataFormat>(data);
+  auto message = make_shared<WriteRequest>(endpoint_, id_, payload);
+
+  return requester_->requestAction(message);
+}
+} // namespace LwM2M
