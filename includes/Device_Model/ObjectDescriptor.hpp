8113ecfb0a57ed428f646802c5655c8a3a2daf57
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2022-08-01 17:25:13 +0200

    add ResourceDescriptorDoesNotExist exception, update ObjectDescriptor docu

--- includes/Device_Model/ObjectDescriptor.hpp
+++ includes/Device_Model/ObjectDescriptor.hpp
@@ -10,8 +10,21 @@
 
 namespace LwM2M {
 
-struct ObjectDescriptor {
+struct ResourceDescriptorDoesNotExist : public std::runtime_error {
+  ResourceDescriptorDoesNotExist(uint32_t object_id, uint32_t resource_id)
+      : runtime_error("Object with id " + std::to_string(object_id) +
+            " does not have a Resource Descriptor with id " +
+            std::to_string(resource_id)) {}
+};
 
+/**
+ * @brief Contains static information of an Object or its instances and a list
+ * or supported Resources.
+ *
+ * Used to create a representation of link object instances and their resources.
+ *
+ */
+struct ObjectDescriptor {
   const std::string name_;
   const std::string description_;
   const uint32_t id_;
@@ -20,13 +33,53 @@ struct ObjectDescriptor {
   const std::string urn_;
   const std::unordered_map<uint32_t, ResourceDescriptorPtr> resources_;
 
+  /**
+   * @brief Creates a default object descriptor. Used for testing only
+   *
+   */
   ObjectDescriptor();
+
+  /**
+   * @brief Copy constructor
+   *
+   * @param instance
+   */
   ObjectDescriptor(const ObjectDescriptor& instance);
+
+  /**
+   * @brief Move constructor
+   *
+   * @param instance
+   */
   ObjectDescriptor(ObjectDescriptor&& instance);
+
+  /**
+   * @brief Creates a valid Object descriptor
+   *
+   * @param name - object name
+   * @param description - human readable description of the object
+   * @param id - object id
+   * @param multiple_instances - does this object support multiple instances of
+   * itself?
+   * @param mandatory - is this resource mandatory for a valid Device instance?
+   * @param urn - OMA LwM2M Registry Uniform Resource Name
+   * @param resources - an index list of supported Resources
+   */
   ObjectDescriptor(std::string name, std::string description, uint32_t id,
       bool multiple_instances, bool mandatory, std::string urn,
       std::unordered_map<uint32_t, ResourceDescriptorPtr> resources);
 
+  /**
+   * @brief A helper to find a given ResourceDescriptorPtr from the contained
+   * resources list
+   *
+   * @param id - index of the requested ResourceDescriptorPtr
+   *
+   * @throws ResourceDescriptorDoesNotExist if given id does not correspond to a
+   * contained resource descriptor
+   *
+   *  @return ResourceDescriptorPtr
+   */
   ResourceDescriptorPtr getResource(uint32_t id);
 };
 
