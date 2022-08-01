80ff1adf9fb4fa11a2f5da0af579e5f58240d164
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2022-08-01 17:26:22 +0200

    update RangeEnumeration docu

--- includes/Device_Model/RangeEnumeration.hpp
+++ includes/Device_Model/RangeEnumeration.hpp
@@ -7,7 +7,7 @@
 
 namespace LwM2M {
 /**
- * @brief RangeEnumeration - represents a range of supported value types by the
+ * @brief Represents a range of supported value types by the
  * LwM2M Resource
  *
  */
@@ -30,13 +30,67 @@ class RangeEnumeration {
   std::optional<FloatRange> float_range_;
 
 public:
+  /**
+   * @brief Creates a default RangeEnumeration, used for testing only.
+   *
+   */
   RangeEnumeration();
+
+  /**
+   * @brief Creates a range enumeration from a given list of string literals
+   *
+   * @param available_values
+   */
   RangeEnumeration(std::vector<std::string> available_values);
+
+  /**
+   * @brief Creates a min-max range based on given integers
+   *
+   * @param minimum_value
+   * @param maximum_value
+   */
   RangeEnumeration(long int minimum_value, long int maximum_value);
+
+  /**
+   * @brief Creates a min-max range based on given decimal values
+   *
+   * @param minimum_value
+   * @param maximum_value
+   */
   RangeEnumeration(double minimum_value, double maximum_value);
 
+  /**
+   * @brief Checks if a given string literal is within the RangeEnumeration
+   * declaration
+   *
+   * @param value - string literal to check against
+   *
+   * @return true - if string literal is within the declared range
+   * @return false - if string literal is outside of the declared range (also if
+   * the range is integer or decimal based)
+   */
   bool inRange(std::string value);
+
+  /**
+   * @brief Checks if a given integer is within the RangeEnumeration declaration
+   *
+   * @param value - integer value to check against
+   *
+   * @return true - if given integer is within the declared range
+   * @return false - if given integer is outside the declared range (or if the
+   * range is based on string literals)
+   */
   bool inRange(long int value);
+
+  /**
+   * @brief Checks if a given decimal is within the RangeEnumeration declaration
+   *
+   * @param value - decimal value to check against
+   *
+   * @return true - if given decimal is within the declared range
+   * @return false - if given decimal is outside the declared range (or if the
+   * range is based on string literals)
+   */
   bool inRange(double value);
 };
 } // namespace LwM2M
