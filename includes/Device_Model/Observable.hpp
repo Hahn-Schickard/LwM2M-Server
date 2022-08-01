e2abafbd97fbfe3f4d347185738fac1392550de5
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2022-08-01 17:25:23 +0200

    update Observable docu

--- includes/Device_Model/Observable.hpp
+++ includes/Device_Model/Observable.hpp
@@ -17,29 +17,103 @@ struct ElementNotObserved : public std::runtime_error {
             " is not in the list of compoisite observed elements.") {}
 };
 
+/**
+ * @brief Models the observation functionality of a resource
+ *
+ */
 struct Observable : public Event_Model::EventSource<PayloadData>,
                     public ElementAddress {
   using ExceptionHandler = std::function<void(std::exception_ptr)>;
   using ObservedDataTypes = std::map<ElementID, DataType>;
 
+  /**
+   * @brief Creates an observable element of various sub-elements
+   *
+   * @param handler - exception handling mechanism
+   * @param requester - observation interface to the related binding
+   * @param endpoint - endpoint address of the modeled entity
+   * @param id - element id of the modeled entity
+   * @param data_types - a list of observed data types indexed by associated
+   * ElementIDs
+   */
   Observable(ExceptionHandler handler, ObservableInterfacePtr requester,
       EndpointPtr endpoint, ElementID id, ObservedDataTypes data_types);
+
+  /**
+   * @brief Creates an observable element for a given ResourceInstance
+   *
+   * @param handler - exception handling mechanism
+   * @param requester - observation interface to the related binding
+   * @param endpoint - endpoint address of the modeled entity
+   * @param id - element id of the modeled entity
+   * @param data_type - observed data type
+   */
   Observable(ExceptionHandler handler, ObservableInterfacePtr requester,
       EndpointPtr endpoint, ElementID id, DataType data_type);
 
   ~Observable();
 
+  /**
+   * @brief Returns an index list of observable elements and their datatypes
+   *
+   * @return ObservedDataTypes
+   */
   ObservedDataTypes getObservedDataTypes();
+
+  /**
+   * @brief Returns the DataType of a given Observable element
+   *
+   * @param id - id of the requested observable element
+   *
+   * @throws ElementNotObserved if given id is not found within the list of
+   * Observable elements
+   *
+   * @return DataType
+   */
   DataType getDataType(ElementID id);
 
 private:
+  /**
+   * @brief Internal method used to send an observation request to the client.
+   *
+   * Issued on first call of attach(), after the internal listeners_ map is
+   * empty
+   *
+   */
   void requestObserver();
 
+  /**
+   * @brief Internal method used to send a request to cancel Observation to the
+   * Client.
+   *
+   * Issued when the last ObserverInterface implementer has called the detach()
+   * method
+   *
+   */
   void cancelObserver();
 
+  /**
+   * @brief Implementation of Event_Model::EventSource<PayloadData>::attach()
+   * method.
+   *
+   * Used by ObserverInterface constructor to add a new observer
+   *
+   * @param listener_callback - callback to
+   * ObserverInterface::handleEvent(std::shared_ptr<LwM2M::PayloadData>)
+   * implementation
+   * @return size_t - observer_id
+   */
   size_t attach(
       Event_Model::HandleEventCallback<PayloadData>&& listener_callback) final;
 
+  /**
+   * @brief Implementation of Event_Model::EventSource<PayloadData>::detach()
+   * method.
+   *
+   * Used by ObserverInterface destructor to remove an observer
+   *
+   * @param callback_id - observer_id_ of ObserverInterface
+   */
   void detach(size_t callback_id) final;
 
   ObservableInterfacePtr requester_;
@@ -50,6 +124,14 @@ private:
 using ObservableSharedPtr = std::shared_ptr<Observable>;
 using ObservablePtr = NonemptyPointer::NonemptyPtr<ObservableSharedPtr>;
 
+/**
+ * @brief A helper class, used to create observers for a given Observable data
+ * source
+ *
+ * Inheritors of this class MUST implement method:
+ * void handleEvent(std::shared_ptr<LwM2M::PayloadData>);
+ *
+ */
 struct ObserverInterface
     : Event_Model::EventListenerInterface<LwM2M::PayloadData> {
   ObserverInterface(ObservablePtr source);
