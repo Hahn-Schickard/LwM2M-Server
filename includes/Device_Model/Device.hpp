7c845c198de46ec100ac5ca88617b9e37315ace4
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2022-08-01 17:35:09 +0200

    update Device docu

--- includes/Device_Model/Device.hpp
+++ includes/Device_Model/Device.hpp
@@ -39,6 +39,10 @@ struct ObjectDoesNotExist : public std::runtime_error {
       : runtime_error("Object " + id.toString() + " does not exits") {}
 };
 
+/**
+ * @brief Models LwM2M Client instance
+ *
+ */
 class Device {
   Observable::ExceptionHandler exception_handler_;
   RequesterInterfaceFacadePtr requester_;
@@ -54,6 +58,10 @@ class Device {
   void makeObjects(ObjectDescriptorsMap object_descriptors_map);
 
 public:
+  /**
+   * @brief Creates a default LwM2M Device, used for testing only.
+   *
+   */
   Device() = default;
   Device(Observable::ExceptionHandler handler,
       RequesterInterfaceFacadePtr requester_, EndpointPtr endpoint,
@@ -62,19 +70,153 @@ public:
       LwM2M_Version version = LwM2M_Version::V1_0,
       BindingType binding = BindingType::UDP, bool queue_mode = false);
 
+  /**
+   * @brief Returns the assigned LwM2M Client ID
+   *
+   * @return std::string
+   */
   std::string getDeviceId();
+
+  /**
+   * @brief Returns the name of LwM2M Client
+   *
+   * @return std::string
+   */
   std::string getName();
+
+  /**
+   * @brief Looks for a given Object based on a given ObjectID
+   *
+   * @param id - id of the requested Object
+   *
+   * @throws ObjectDoesNotExist if given ElementID does not address a contained
+   * Object
+   *
+   * @return ObjectPtr
+   */
   ObjectPtr getObject(ElementID id);
+
+  /**
+   * @brief Returns all linked objects of this Device
+   *
+   * @return ObjectsMap
+   */
   ObjectsMap getObjects();
+
+  /**
+   * @brief Looks for a given ObjectInstance based on a given ELementID
+   *
+   * @param id - id of the requested ObjectInstance
+   *
+   * @throws ObjectDoesNotExist if given ElementID does not address a contained
+   * Object
+   * @throws ObjectInstanceDoesNotExist if given ElementID does not address a
+   * contained Object Instance
+   *
+   * @return ObjectInstancePtr
+   */
   ObjectInstancePtr getObjectInstance(ElementID id);
+
+  /**
+   * @brief Returns all Object instances linked to the given Object
+   *
+   * @param id - id of the requested Object
+   *
+   * @throws ObjectDoesNotExist if given ElementID does not address a contained
+   * Object
+   *
+   * @return Resources
+   */
   ObjectInstances getObjectInstances(ElementID id);
+
+  /**
+   * @brief Looks for a given Resource based on a given ElementID
+   *
+   * @param id - id of the requested Resource.
+   *
+   * @throws ObjectDoesNotExist if given ElementID does not address a contained
+   * Object
+   * @throws ObjectInstanceDoesNotExist if given ElementID does not address a
+   * contained Object Instance
+   * @throws ResourceDoesNotExist if given ElementID does not address a
+   * contained Resource
+   *
+   * @return ResourcePtr
+   */
   ResourcePtr getResource(ElementID id);
+
+  /**
+   * @brief Returns all Resources linked to the given Object Instance
+   *
+   * @param id - id of the requested ObjectInstance
+   *
+   * @throws ObjectDoesNotExist if given ElementID does not address a contained
+   * Object
+   * @throws ObjectInstanceDoesNotExist if given ElementID does not address a
+   * contained Object Instance
+   *
+   * @return Resources
+   */
   Resources getResources(ElementID id);
+
+  /**
+   * @brief Looks for a given ResourceInstance based on a given ElementID
+   *
+   * @param id - id of the requested ResourceInstance. If the current resource
+   * does not support multiple resource instances, Resource ID will be used
+   * instead of a Resource instance ID
+   *
+   * @throws ObjectDoesNotExist if given ElementID does not address a contained
+   * Object
+   * @throws ObjectInstanceDoesNotExist if given ElementID does not address a
+   * contained Object Instance
+   * @throws ResourceDoesNotExist if given ElementID does not address a
+   * contained Resource
+   * @throws ResourceInstanceDoesNotExist if given ElementID could not be found
+   * within current ResourceInstances field
+   *
+   * @return ResourceInstance - @see ResourceInstance on how to obtain stored
+   * information
+   */
   ResourceInstance getResourceInstance(ElementID id);
+
+  /**
+   * @brief  Returns all ResourceInstances linked to given Resource
+   *
+   * @param id - id of a given Resource, from which ResourceInstances should be
+   * obtained
+   *
+   * @throws ObjectDoesNotExist if given ElementID does not address a contained
+   * Object
+   * @throws ObjectInstanceDoesNotExist if given ElementID does not address a
+   * contained Object Instance
+   * @throws ResourceDoesNotExist if given ElementID does not address a
+   * contained Resource
+   *
+   * @return ResourceInstances
+   */
   ResourceInstances getResourceInstances(ElementID id);
 
+  /**
+   * @brief Used by the managing binding to change the Client BindingType
+   *
+   * @param binding
+   */
   void updateBinding(BindingType binding);
+
+  /**
+   * @brief Used by the managing binding to change the Client lifetime
+   *
+   * @param life_time
+   */
   void updateLifetime(size_t life_time);
+
+  /**
+   * @brief Used by the managing binding to change the assigned object
+   * descriptors
+   *
+   * @param object_instances
+   */
   void updateObjectsMap(ObjectDescriptorsMap object_instances);
 };
 
