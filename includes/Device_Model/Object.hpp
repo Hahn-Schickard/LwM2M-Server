66b291ec6fd6a05cfba69ddfa9965ea3ab4afe86
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2022-08-01 17:27:58 +0200

    update Object docu

--- includes/Device_Model/Object.hpp
+++ includes/Device_Model/Object.hpp
@@ -41,6 +41,11 @@ struct ObjectInstanceDoesNotExist : public std::runtime_error {
       : runtime_error("Object instance " + id.toString() + " does not exist") {}
 };
 
+/**
+ * @brief Models a collection of data points and their groupings, all described
+ * by a same object descriptor
+ *
+ */
 class Object {
   RequesterInterfaceFacadePtr requester_;
   EndpointPtr endpoint_;
@@ -52,12 +57,92 @@ public:
       RequesterInterfaceFacadePtr requester, EndpointPtr endpoint,
       RequiredObjectInstances instances, ObjectDescriptorPtr descriptor);
 
+  /**
+   * @brief Returns static information of this object.
+   *
+   * Used to display the linked object instances and set resource descriptors.
+   *
+   * @return ObjectDescriptorPtr
+   */
   ObjectDescriptorPtr getDescriptor();
+
+  /**
+   * @brief Looks for a given Object instance based on a given ELementID
+   *
+   * @param id - id of the requested ObjectInstance
+   *
+   * @throws ObjectInstanceDoesNotExist if given ElementID does not address a
+   * contained Object Instance
+   *
+   * @return ObjectInstancePtr
+   */
   ObjectInstancePtr getObjectInstance(ElementID id);
+
+  /**
+   * @brief Returns all linked Object instances of this Object
+   *
+   * @return ObjectInstances
+   */
   ObjectInstances getObjectInstances();
+
+  /**
+   * @brief Looks for a given Resource based on a given ElementID
+   *
+   * @param id - id of the requested Resource.
+   *
+   * @throws ObjectInstanceDoesNotExist if given ElementID does not address a
+   * contained Object Instance
+   * @throws ResourceDoesNotExist if given ElementID does not address a
+   * contained Resource
+   *
+   * @return ResourcePtr
+   */
   ResourcePtr getResource(ElementID id);
+
+  /**
+   * @brief Returns all Resources linked to the given Object Instance
+   *
+   * @param id - id of the requested ObjectInstance
+   *
+   * @throws ObjectInstanceDoesNotExist if given ElementID does not address a
+   * contained Object Instance
+   *
+   * @return Resources
+   */
   Resources getResources(ElementID id);
+
+  /**
+   * @brief Looks for a given ResourceInstance based on a given ElementID
+   *
+   * @param id - id of the requested ResourceInstance. If the current resource
+   * does not support multiple resource instances, Resource ID will be used
+   * instead of a Resource instance ID
+   *
+   * @throws ObjectInstanceDoesNotExist if given ElementID does not address a
+   * contained Object Instance
+   * @throws ResourceDoesNotExist if given ElementID does not address a
+   * contained Resource
+   * @throws ResourceInstanceDoesNotExist if given ElementID could not be found
+   * within current ResourceInstances field
+   *
+   * @return ResourceInstance - @see ResourceInstance on how to obtain stored
+   * information
+   */
   ResourceInstance getResourceInstance(ElementID id);
+
+  /**
+   * @brief  Returns all ResourceInstances linked to given Resource
+   *
+   * @param id - id of a given Resource, from which ResourceInstances should be
+   * obtained
+   *
+   * @throws ObjectInstanceDoesNotExist if given ElementID does not address a
+   * contained Object Instance
+   * @throws ResourceDoesNotExist if given ElementID does not address a
+   * contained Resource
+   *
+   * @return ResourceInstances
+   */
   ResourceInstances getResourceInstances(ElementID id);
 };
 
