67c73a3644a38b7bcf48c89455c412cdc682aba4
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2022-08-01 16:00:16 +0200

    add docu to CancelableInterface and RequestResult classes

--- includes/Device_Model/CancelableInterface.hpp
+++ includes/Device_Model/CancelableInterface.hpp
@@ -13,6 +13,12 @@
 
 namespace LwM2M {
 
+/**
+ * @brief Models a cancelable request result, issued by CancelableInterface
+ * inheritors
+ *
+ * @tparam T - promised result type
+ */
 template <typename T> struct RequestResult {
   using Functor = std::function<void(size_t)>;
   using RequestCleanerPtr = Threadsafe::WeakPtr<Functor>;
@@ -25,6 +31,12 @@ template <typename T> struct RequestResult {
         cleanupRequest_(cleanupRequest_callback),
         cancelRequest_(cancelRequest_callback) {}
 
+  /**
+   * @brief Waits until the promised result is available and cleanups the
+   * cancelable request, once the result has been obtained
+   *
+   * @return T - promised result type
+   */
   T get() {
     auto result = result_.get();
     if (auto cleanupRequest = cleanupRequest_.lock()) {
@@ -34,10 +46,29 @@ template <typename T> struct RequestResult {
     return result;
   }
 
+  /**
+   * @brief Calls the get() method in a separate thread and returns a future to
+   * the promised result
+   *
+   * @return std::future<T>
+   */
   std::future<T> asyncGet() {
     return std::async(std::launch::async, &RequestResult::get, this);
   }
 
+  /**
+   * @brief Cancels the requested result.
+   *
+   * @note The user must call either get() or asyncGet() to handle the promised
+   * result (which will be an exception) after the call to cancel() method. If
+   * this is not done the destructor of RequestResult will throw an exception
+   * when cleaning up the promised result.
+   *
+   * @throws std::runtime_error if linked CancelableInterface callback is no
+   * longer available (this can only happen when linked CancelableInterface was
+   * destroyed)
+   *
+   */
   void cancel() {
     if (auto cancelRequest = cancelRequest_.lock()) {
       (*cancelRequest)(id_);
@@ -53,6 +84,13 @@ private:
   RequestCancelerPtr cancelRequest_;
 };
 
+/**
+ * @brief Models the functionality for cancelable requests. Used by Objects,
+ * Object Instances, Resources and Resource Instances.
+ *
+ * MUST be implemented be the Binding
+ *
+ */
 struct CancelableInterface {
   CancelableInterface()
       : cleaner_(ThreadsafeFunctor::make(
@@ -79,6 +117,16 @@ struct CancelableInterface {
     throw std::runtime_error("Called base cancelRequest implementation.");
   }
 
+  /**
+   * @brief Creates a cancelable request. ONLY used by the Device Model
+   * elements. This method SHOULD not be used by the users
+   *
+   * @tparam T - promised result type
+   * @param request - a cancelable LwM2M Message from the Server to the Client
+   * @param result_future - desired future result type (used when type
+   * conversions are required)
+   * @return RequestResult<T> - cancelable request result
+   */
   template <typename T>
   RequestResult<T> issueCancelable(
       ServerRequestPtr request, std::future<T>&& result_future) {
@@ -87,6 +135,12 @@ struct CancelableInterface {
   }
 
 private:
+  /**
+   * @brief Internal method used to add new cancelable messages
+   *
+   * @param request
+   * @return size_t
+   */
   size_t addRequest(ServerRequestPtr request) {
     std::lock_guard<std::mutex> guard(request_mutex_);
     auto id = requests_.size() + 1;
@@ -94,8 +148,20 @@ private:
     return id;
   }
 
+  /**
+   * @brief Internal method to remove handled cancelable messages. Used as
+   * RequestResult::RequestCleanerPtr callback
+   *
+   * @param request_id
+   */
   void cleanupHandled(size_t request_id) { requests_.erase(request_id); }
 
+  /**
+   * @brief Internal method to cancel a given request result.  Used as
+   * RequestResult::RequestCancelerPtr callback
+   *
+   * @param request_id
+   */
   void cancelIssued(size_t request_id) {
     auto it = requests_.find(request_id);
     if (it != requests_.end()) {
