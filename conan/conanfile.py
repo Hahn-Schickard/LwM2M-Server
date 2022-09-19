8c95112c2923bbd64c47479fae0ad4c2bf92b74f
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2022-09-19 11:23:55 +0200

    do not run code linting while creating a new package

--- conan/conanfile.py
+++ conan/conanfile.py
@@ -60,6 +60,8 @@ class PackageConan(ConanFile):
             return self._cmake
         self._cmake = CMake(self)
         self._cmake.verbose = True
+        self._cmake.definitions["STATIC_CODE_ANALYSIS"] = False
+        self._cmake.definitions["USE_CONAN"] = True
         self._cmake.configure(build_dir=os.path.join(
             self.build_folder, "build"))
         return self._cmake
