2f64fea3cbd4fc158504612f0ff3595827b27dcc
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2022-09-30 11:42:21 +0200

    fix conan integration test

--- conan/test_package/example.cpp
+++ conan/test_package/example.cpp
@@ -1,4 +1,5 @@
 #include "HaSLL/LoggerManager.hpp"
+#include "HaSLL/SPD_LoggerRepository.hpp"
 #include "LwM2M_Server/Server.hpp"
 
 #include <iomanip>
@@ -10,14 +11,13 @@ using namespace std;
 
 int main() {
   {
-    auto config = HaSLL::Configuration(
-        "./log", "logfile.log", "[%Y-%m-%d-%H:%M:%S:%F %z][%n]%^[%l]: %v%$",
+    auto config = HaSLL::SPD_Configuration("./log", "logfile.log",
+        "[%Y-%m-%d-%H:%M:%S:%F %z][%n]%^[%l]: %v%$",
         HaSLL::SeverityLevel::TRACE, false, 8192, 2, 25, 100, 1);
     auto repo = make_shared<SPD_LoggerRepository>(config);
-    LoggerManager::initialise(repo)
+    LoggerManager::initialise(repo);
 
-    auto logger =
-        LoggerManager::registerLogger("Integration_Runner");
+    auto logger = LoggerManager::registerLogger("Integration_Runner");
     LoggerManager::configure(SeverityLevel::TRACE);
 
     unique_ptr<LwM2M::Server> server;
@@ -27,7 +27,7 @@ int main() {
       logger->log(SeverityLevel::INFO, "Started LwM2M Server!");
       this_thread::sleep_for(1s);
 
-    } catch (exception &e) {
+    } catch (exception& e) {
       logger->log(SeverityLevel::ERROR, "Received an exception: {}", e.what());
       cerr << e.what();
     }
