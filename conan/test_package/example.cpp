cdbb3b1ae121b641c83db83bd3b3175d6b1efdcd
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2022-10-05 12:48:36 +0200

    refactor integration test to print out to console and check if all binding types have been linked

--- conan/test_package/example.cpp
+++ conan/test_package/example.cpp
@@ -2,31 +2,28 @@
 #include "HaSLL/SPD_LoggerRepository.hpp"
 #include "LwM2M_Server/Server.hpp"
 
-#include <iomanip>
 #include <iostream>
-#include <thread>
 
 using namespace HaSLL;
 using namespace std;
 
 int main() {
   {
-    auto config = HaSLL::SPD_Configuration("./log", "logfile.log",
-        "[%Y-%m-%d-%H:%M:%S:%F %z][%n]%^[%l]: %v%$",
-        HaSLL::SeverityLevel::TRACE, false, 8192, 2, 25, 100, 1);
-    auto repo = make_shared<SPD_LoggerRepository>(config);
+    auto repo = make_shared<SPD_LoggerRepository>();
     LoggerManager::initialise(repo);
-
-    auto logger = LoggerManager::registerLogger("Integration_Runner");
     LoggerManager::configure(SeverityLevel::TRACE);
+    auto logger = LoggerManager::registerLogger("Integration_Runner");
 
     unique_ptr<LwM2M::Server> server;
     try {
       server = make_unique<LwM2M::Server>("config/serverConfig.json");
+      cout << "LwM2M Server uses the following bindings:" << endl;
+      for (const auto& name : server->getRegisterBindingNames()) {
+        cout << name << endl;
+      }
       server->start();
-      logger->log(SeverityLevel::INFO, "Started LwM2M Server!");
+      cout << "Started LwM2M Server!" << endl;
       this_thread::sleep_for(1s);
-
     } catch (exception& e) {
       logger->log(SeverityLevel::ERROR, "Received an exception: {}", e.what());
       cerr << e.what();
@@ -34,7 +31,7 @@ int main() {
 
     if (server) {
       server->stop();
-      logger->log(SeverityLevel::INFO, "Stopped LwM2M Server!");
+      cout << "Stopped LwM2M Server!" << endl;
     }
   }
 
